9e1d7102337e6a901eb2e0a10af08522a023a85a
Jean-Marc Vanel <jeanmarc.vanel@gmail.com>
2003-03-26 15:53:59 +0000

    Initial revision
    svn path=/trunk/eXist-1.0/; revision=32
    
    
    Former-commit-id: 46ac2b52183ace2ee8076f97cdd111abdd9bad97

---
+++ src/wwbota/xmldb/core/Database.java
@@ -0,0 +1,60 @@
+begin_unit|revision:1.0.0;language:Java;cregit-version:0.0.1
+begin_comment
+comment|/** Java class "Database.java" generated from Poseidon for UML.  *  Poseidon for UML is developed by<A HREF="http://www.gentleware.com">Gentleware</A>.  *  Generated with<A HREF="http://jakarta.apache.org/velocity/">velocity</A> template engine.  */
+end_comment
+
+begin_package
+package|package
+name|wwbota
+operator|.
+name|xmldb
+operator|.
+name|core
+package|;
+end_package
+
+begin_import
+import|import
+name|java
+operator|.
+name|util
+operator|.
+name|*
+import|;
+end_import
+
+begin_comment
+comment|/**  *<p>  *   *</p>  */
+end_comment
+
+begin_class
+specifier|public
+class|class
+name|Database
+block|{
+comment|///////////////////////////////////////
+comment|// operations
+comment|/**  *<p>  * Does ...  *</p><p>  *   * @return a Query with ...  *</p>  */
+specifier|public
+specifier|static
+name|Query
+name|createQuery
+parameter_list|()
+block|{
+comment|// your code here
+return|return
+operator|new
+name|Query
+argument_list|()
+return|;
+block|}
+comment|// end createQuery
+block|}
+end_class
+
+begin_comment
+comment|// end Database
+end_comment
+
+end_unit
+
