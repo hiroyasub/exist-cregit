5d58ce631d40b43cf25d0490538fcfae03cd89df
Wolfgang Meier <wolfgangmm@gmail.com>
2004-08-12 16:11:08 +0000

    Removed class DocumentProxy which is no longer required.
    svn path=/trunk/eXist-1.0/; revision=524
    
    
    Former-commit-id: 783be94d42a16285557bb79881ab3336097d02e5

--- src/org/exist/xmldb/RemoteCollection.java
+++ src/org/exist/xmldb/RemoteCollection.java
@@ -59,16 +59,6 @@ name|java
 operator|.
 name|util
 operator|.
-name|ArrayList
-import|;
-end_import
-
-begin_import
-import|import
-name|java
-operator|.
-name|util
-operator|.
 name|Date
 import|;
 end_import
@@ -100,16 +90,6 @@ operator|.
 name|util
 operator|.
 name|Iterator
-import|;
-end_import
-
-begin_import
-import|import
-name|java
-operator|.
-name|util
-operator|.
-name|List
 import|;
 end_import
 
