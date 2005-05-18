abed7faeac95517cfc4951d7cca49f4b489c53cc
Dannes Wessels <dannes@exist-db.org>
2005-05-18 18:10:42 +0000

    Added CVS tag to header. Removed import statement. Removed doPost() method.
    svn path=/trunk/eXist-1.0/; revision=1239
    
    
    Former-commit-id: 45d46d0b53b1596adf77388a9bbe9fce4a85e82b

--- src/org/exist/http/servlets/Log4jInit.java
+++ src/org/exist/http/servlets/Log4jInit.java
@@ -1,6 +1,6 @@
 begin_unit|revision:1.0.0;language:Java;cregit-version:0.0.1
 begin_comment
-comment|/*  *  eXist Open Source Native XML Database  *  Copyright (C) 2001-03 Wolfgang M. Meier  *  wolfgang@exist-db.org  *  http://exist.sourceforge.net  *  *  This program is free software; you can redistribute it and/or  *  modify it under the terms of the GNU Lesser General Public License  *  as published by the Free Software Foundation; either version 2  *  of the License, or (at your option) any later version.  *  *  This program is distributed in the hope that it will be useful,  *  but WITHOUT ANY WARRANTY; without even the implied warranty of  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the  *  GNU Lesser General Public License for more details.  *  *  You should have received a copy of the GNU Lesser General Public License  *  along with this program; if not, write to the Free Software  *  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.  *  *  TODO: Add CVS tag.  */
+comment|/*  *  eXist Open Source Native XML Database  *  Copyright (C) 2001-03 Wolfgang M. Meier  *  wolfgang@exist-db.org  *  http://exist.sourceforge.net  *  *  This program is free software; you can redistribute it and/or  *  modify it under the terms of the GNU Lesser General Public License  *  as published by the Free Software Foundation; either version 2  *  of the License, or (at your option) any later version.  *  *  This program is distributed in the hope that it will be useful,  *  but WITHOUT ANY WARRANTY; without even the implied warranty of  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the  *  GNU Lesser General Public License for more details.  *  *  You should have received a copy of the GNU Lesser General Public License  *  along with this program; if not, write to the Free Software  *  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.  *  *  $Id$  */
 end_comment
 
 begin_package
@@ -22,16 +22,6 @@ operator|.
 name|io
 operator|.
 name|File
-import|;
-end_import
-
-begin_import
-import|import
-name|java
-operator|.
-name|io
-operator|.
-name|IOException
 import|;
 end_import
 
@@ -220,20 +210,6 @@ comment|/**      *  Empty method.      *      * @param req HTTP Request object  
 specifier|public
 name|void
 name|doGet
-parameter_list|(
-name|HttpServletRequest
-name|req
-parameter_list|,
-name|HttpServletResponse
-name|res
-parameter_list|)
-block|{
-comment|//
-block|}
-comment|/**      *  Empty method.      *      * @param req HTTP Request object      * @param res HTTP Response object      */
-specifier|public
-name|void
-name|doPost
 parameter_list|(
 name|HttpServletRequest
 name|req
