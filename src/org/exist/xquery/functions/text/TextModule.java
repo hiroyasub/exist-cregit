f6359d42ad8346b1d04c7424a11506434b8fc48d
Wolfgang Meier <wolfgangmm@gmail.com>
2005-07-14 21:42:09 +0000

    *** empty log message ***
    svn path=/trunk/eXist-1.0/; revision=1416
    
    
    Former-commit-id: 7909a170988bd1d9ea45e053636bb3e3baf45616

--- src/org/exist/xquery/functions/text/TextModule.java
+++ src/org/exist/xquery/functions/text/TextModule.java
@@ -147,6 +147,18 @@ name|IndexTerms
 operator|.
 name|class
 argument_list|)
+block|,
+operator|new
+name|FunctionDef
+argument_list|(
+name|HighlightMatches
+operator|.
+name|signature
+argument_list|,
+name|HighlightMatches
+operator|.
+name|class
+argument_list|)
 block|}
 decl_stmt|;
 comment|/** 	 * @param functions 	 */
