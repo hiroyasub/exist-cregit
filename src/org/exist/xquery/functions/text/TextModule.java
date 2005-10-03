a29a0befce0f8c1092aae4c44c1bfc2b8cd655ee
Dannes Wessels <dannes@exist-db.org>
2005-10-03 20:31:39 +0000

    Added function text:filter(string, regexp). Usefull for filtering substrings that match regexp pattern in larger string
    svn path=/trunk/eXist-1.0/; revision=1775
    
    
    Former-commit-id: 6763487e8547ff6e9b321228592252dfa3dc763f

--- src/org/exist/xquery/functions/text/TextModule.java
+++ src/org/exist/xquery/functions/text/TextModule.java
@@ -1,6 +1,6 @@
 begin_unit|revision:1.0.0;language:Java;cregit-version:0.0.1
 begin_comment
-comment|/*  *  eXist Open Source Native XML Database  *  Copyright (C) 2001-03 Wolfgang M. Meier  *  wolfgang@exist-db.org  *  http://exist.sourceforge.net  *    *  This program is free software; you can redistribute it and/or  *  modify it under the terms of the GNU Lesser General Public License  *  as published by the Free Software Foundation; either version 2  *  of the License, or (at your option) any later version.  *    *  This program is distributed in the hope that it will be useful,  *  but WITHOUT ANY WARRANTY; without even the implied warranty of  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the  *  GNU Lesser General Public License for more details.  *    *  You should have received a copy of the GNU Lesser General Public License  *  along with this program; if not, write to the Free Software  *  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.  *    *  $Id$  */
+comment|/*  *  eXist Open Source Native XML Database  *  Copyright (C) 2001-03 Wolfgang M. Meier  *  wolfgang@exist-db.org  *  http://exist.sourceforge.net  *  *  This program is free software; you can redistribute it and/or  *  modify it under the terms of the GNU Lesser General Public License  *  as published by the Free Software Foundation; either version 2  *  of the License, or (at your option) any later version.  *  *  This program is distributed in the hope that it will be useful,  *  but WITHOUT ANY WARRANTY; without even the implied warranty of  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the  *  GNU Lesser General Public License for more details.  *  *  You should have received a copy of the GNU Lesser General Public License  *  along with this program; if not, write to the Free Software  *  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.  *  *  $Id$  */
 end_comment
 
 begin_package
@@ -189,9 +189,21 @@ name|KWICDisplay
 operator|.
 name|class
 argument_list|)
+block|,
+operator|new
+name|FunctionDef
+argument_list|(
+name|RegexpFilter
+operator|.
+name|signature
+argument_list|,
+name|RegexpFilter
+operator|.
+name|class
+argument_list|)
 block|}
 decl_stmt|;
-comment|/** 	 * @param functions 	 */
+comment|/**      * @param functions      */
 specifier|public
 name|TextModule
 parameter_list|()
@@ -202,7 +214,7 @@ name|functions
 argument_list|)
 expr_stmt|;
 block|}
-comment|/* (non-Javadoc) 	 * @see org.exist.xquery.Module#getDescription() 	 */
+comment|/* (non-Javadoc)          * @see org.exist.xquery.Module#getDescription()          */
 specifier|public
 name|String
 name|getDescription
@@ -212,7 +224,7 @@ return|return
 literal|"Extension functions for text searching"
 return|;
 block|}
-comment|/* (non-Javadoc) 	 * @see org.exist.xquery.Module#getNamespaceURI() 	 */
+comment|/* (non-Javadoc)          * @see org.exist.xquery.Module#getNamespaceURI()          */
 specifier|public
 name|String
 name|getNamespaceURI
@@ -222,7 +234,7 @@ return|return
 name|NAMESPACE_URI
 return|;
 block|}
-comment|/* (non-Javadoc) 	 * @see org.exist.xquery.Module#getDefaultPrefix() 	 */
+comment|/* (non-Javadoc)          * @see org.exist.xquery.Module#getDefaultPrefix()          */
 specifier|public
 name|String
 name|getDefaultPrefix
