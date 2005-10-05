1b27006fbcf9ec8015197b49070050cffbc3e9b8
Wolfgang Meier <wolfgangmm@gmail.com>
2005-10-05 20:33:14 +0000

    New function, request:stream-binary(), streams the binary data passed in its argument to the current servlet request output stream. The data is expected to be of type xs:base64Binary. The function can be used to read images out of the database and directly return them from an XQuery.
    svn path=/trunk/eXist-1.0/; revision=1788
    
    
    Former-commit-id: fb445ad0fa62e7e892e94a0faf3a86f51b374ba7

--- src/org/exist/xquery/functions/request/RequestModule.java
+++ src/org/exist/xquery/functions/request/RequestModule.java
@@ -387,7 +387,19 @@ name|SetDateHeader
 operator|.
 name|class
 argument_list|)
-block|, 	}
+block|,
+operator|new
+name|FunctionDef
+argument_list|(
+name|StreamBinary
+operator|.
+name|signature
+argument_list|,
+name|StreamBinary
+operator|.
+name|class
+argument_list|)
+block|}
 decl_stmt|;
 specifier|public
 name|RequestModule
