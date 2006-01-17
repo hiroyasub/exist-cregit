5ac54841aa87d343216d8c2aba811b6aed6f7ba4
Pierrick Brihaye <pierrick.brihaye@free.fr>
2006-01-17 16:51:41 +0000

    Retrieving root nodes of in Memory documents was leading to serious memory errors. Root nodes of temporary documents fragments were wrong.
    svn path=/trunk/eXist-1.0/; revision=2507
    
    
    Former-commit-id: bd45d720e2b87d9d6a08a351334ac7e0c34ba7cf

--- src/org/exist/memtree/DocumentImpl.java
+++ src/org/exist/memtree/DocumentImpl.java
@@ -2728,6 +2728,20 @@ operator|.
 name|nodeNumber
 condition|)
 break|break;
+comment|//No nextNode if the top node is a Document node
+if|if
+condition|(
+name|top
+operator|!=
+literal|null
+operator|&&
+name|top
+operator|.
+name|nodeNumber
+operator|==
+literal|0
+condition|)
+break|break;
 name|nextNode
 operator|=
 operator|(
