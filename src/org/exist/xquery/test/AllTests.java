6fdd0a16ec4380b0fd69a85eae1d247ede0e69df
Wolfgang Meier <wolfgangmm@gmail.com>
2006-03-11 19:18:51 +0000

    New test to check if cached objects are properly cleared after an update.
    svn path=/trunk/eXist-1.0/; revision=2874
    
    
    Former-commit-id: 2fb51ba2f752798b59e6e9858ddc5d48bc93f96b

--- src/org/exist/xquery/test/AllTests.java
+++ src/org/exist/xquery/test/AllTests.java
@@ -197,6 +197,15 @@ operator|.
 name|class
 argument_list|)
 expr_stmt|;
+name|suite
+operator|.
+name|addTestSuite
+argument_list|(
+name|DocumentUpdateTest
+operator|.
+name|class
+argument_list|)
+expr_stmt|;
 comment|//		suite.addTestSuite(XQueryUseCasesTest.class);
 comment|//$JUnit-END$
 return|return
