f13004de8ae3fe3d50f99d231527778b8f0fe319
Dannes Wessels <dannes@exist-db.org>
2006-03-16 08:30:47 +0000

    Tests to prove 0 byte binary store issue
    svn path=/trunk/eXist-1.0/; revision=2899
    
    
    Former-commit-id: 369119a52698f658a6ba608c6b8379a90eb7c1d7

--- src/org/exist/storage/test/AllTests.java
+++ src/org/exist/storage/test/AllTests.java
@@ -214,6 +214,15 @@ operator|.
 name|class
 argument_list|)
 expr_stmt|;
+name|suite
+operator|.
+name|addTestSuite
+argument_list|(
+name|ResourceTest
+operator|.
+name|class
+argument_list|)
+expr_stmt|;
 comment|//$JUnit-END$
 return|return
 name|suite
