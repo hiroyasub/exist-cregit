671af6c834f7f0d9a75c42a5a97fb6502901ea3f
Wolfgang Meier <wolfgangmm@gmail.com>
2006-03-23 16:41:15 +0000

    Test collection names containing escaped characters.
    svn path=/trunk/eXist-1.0/; revision=2949
    
    
    Former-commit-id: fb77cc8cd6a39690e6bae8a7da48305dfb6ea4a0

--- src/org/exist/xmldb/test/LocalTests.java
+++ src/org/exist/xmldb/test/LocalTests.java
@@ -137,6 +137,15 @@ operator|.
 name|class
 argument_list|)
 expr_stmt|;
+name|suite
+operator|.
+name|addTestSuite
+argument_list|(
+name|CollectionTest
+operator|.
+name|class
+argument_list|)
+expr_stmt|;
 comment|//$JUnit-END$
 return|return
 name|suite
