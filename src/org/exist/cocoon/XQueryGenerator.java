b407f224c03527ed071ceb25bb614e252db2ebec
Adam Retter <adam.retter@googlemail.com>
2006-04-09 19:26:17 +0000

    Split the large request module into three modules - request, response and session
    svn path=/trunk/eXist-1.0/; revision=3122
    
    
    Former-commit-id: 2f97e3d69982346d27f13f69542b4126a95157fb

--- src/org/exist/cocoon/XQueryGenerator.java
+++ src/org/exist/cocoon/XQueryGenerator.java
@@ -1,6 +1,6 @@
 begin_unit|revision:1.0.0;language:Java;cregit-version:0.0.1
 begin_comment
-comment|/*  *  eXist Open Source Native XML Database  *  Copyright (C) 2001-06 Wolfgang M. Meier  *  wolfgang@exist-db.org  *  http://exist.sourceforge.net  *    *  This program is free software; you can redistribute it and/or  *  modify it under the terms of the GNU Lesser General Public License  *  as published by the Free Software Foundation; either version 2  *  of the License, or (at your option) any later version.  *    *  This program is distributed in the hope that it will be useful,  *  but WITHOUT ANY WARRANTY; without even the implied warranty of  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the  *  GNU Lesser General Public License for more details.  *    *  You should have received a copy of the GNU Lesser General Public License  *  along with this program; if not, write to the Free Software  *  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.  *    *  $Id$  */
+comment|/*  *  eXist Open Source Native XML Database  *  Copyright (C) 2001-06 Wolfgang M. Meier  *  wolfgang@exist-db.org  *  http://exist.sourceforge.net  *    *  This program is free software; you can redistribute it and/or  *  modify it under the terms of the GNU Lesser General Public License  *  as published by the Free Software Foundation; either version 2  *  of the License, or (at your option) any later version.  *    *  This program is distributed in the hope that it will be useful,  *  but WITHOUT ANY WARRANTY; without even the implied warranty of  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the  *  GNU Lesser General Public License for more details.  *    *  You should have received a copy of the GNU Lesser General Public License  *  along with this program; if not, write to the Free Software  *  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.  *    *  $id$  */
 end_comment
 
 begin_package
@@ -524,6 +524,38 @@ operator|.
 name|request
 operator|.
 name|RequestModule
+import|;
+end_import
+
+begin_import
+import|import
+name|org
+operator|.
+name|exist
+operator|.
+name|xquery
+operator|.
+name|functions
+operator|.
+name|response
+operator|.
+name|ResponseModule
+import|;
+end_import
+
+begin_import
+import|import
+name|org
+operator|.
+name|exist
+operator|.
+name|xquery
+operator|.
+name|functions
+operator|.
+name|session
+operator|.
+name|SessionModule
 import|;
 end_import
 
@@ -1644,7 +1676,7 @@ name|service
 operator|.
 name|declareVariable
 argument_list|(
-name|RequestModule
+name|ResponseModule
 operator|.
 name|PREFIX
 operator|+
@@ -1667,7 +1699,7 @@ name|service
 operator|.
 name|declareVariable
 argument_list|(
-name|RequestModule
+name|SessionModule
 operator|.
 name|PREFIX
 operator|+
