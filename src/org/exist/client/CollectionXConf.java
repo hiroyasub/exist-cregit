ba93248d1a956b33b2e0f048801a4dee7bf0fdd6
Adam Retter <adam.retter@googlemail.com>
2006-05-04 10:43:03 +0000

    Further removal of Vector in favour of ArrayList
    svn path=/trunk/eXist-1.0/; revision=3400
    
    
    Former-commit-id: 84bef999d86253aad3aaf75bb477d46903de2580

--- src/org/exist/client/CollectionXConf.java
+++ src/org/exist/client/CollectionXConf.java
@@ -1,4 +1,8 @@
 begin_unit|revision:1.0.0;language:Java;cregit-version:0.0.1
+begin_comment
+comment|/*  * eXist Open Source Native XML Database  *  * Copyright (C) 2001-06 Wolfgang M. Meier wolfgang@exist-db.org  *  * This program is free software; you can redistribute it and/or modify it  * under the terms of the GNU Lesser General Public License as published by the  * Free Software Foundation; either version 2 of the License, or (at your  * option) any later version.  *  * This program is distributed in the hope that it will be useful, but WITHOUT  * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or  * FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License  * for more details.  *  * You should have received a copy of the GNU Lesser General Public License  * along with this program; if not, write to the Free Software Foundation,  * Inc., 675 Mass Ave, Cambridge, MA 02139, USA.  *  * $Id  */
+end_comment
+
 begin_package
 package|package
 name|org
@@ -178,11 +182,7 @@ import|;
 end_import
 
 begin_comment
-comment|/**  * @author adam  *   * */
-end_comment
-
-begin_comment
-comment|//class to represent a collection.xconf
+comment|/**  * Class to represent a collection.xconf   *   * @author Adam Retter<adam.retter@devon.gov.uk>  * @serial 2006-05-04  * @version 1.0  */
 end_comment
 
 begin_class
