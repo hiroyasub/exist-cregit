d346f44c541a0cf0f2a4f88224cd6fd7a7cf28e3
alexmilowski <alex@milowski.com>
2006-06-29 17:08:34 +0000

    Switched to using JUG for UUID generation for JDK 1.4 compatibility. Note: standalone now has a dependency on jug-lgpl-*.jar
    svn path=/trunk/eXist-1.0/; revision=3755
    
    
    Former-commit-id: abc14bae9ebce6b2a9f0dd8bad3c1f0275f18702

--- src/org/exist/atom/modules/AtomFeeds.java
+++ src/org/exist/atom/modules/AtomFeeds.java
@@ -107,16 +107,6 @@ end_import
 
 begin_import
 import|import
-name|java
-operator|.
-name|util
-operator|.
-name|UUID
-import|;
-end_import
-
-begin_import
-import|import
 name|javax
 operator|.
 name|servlet
