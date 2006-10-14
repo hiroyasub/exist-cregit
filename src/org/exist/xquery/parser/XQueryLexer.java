cbe549c4b60c103d36d937ca39b6602e5151cad0
Wolfgang Meier <wolfgangmm@gmail.com>
2006-10-14 15:01:17 +0000

    Imported revisions 4597 to 4605 from trunk.
    svn path=/branches/DLN/eXist-1.0/; revision=4607
    
    
    Former-commit-id: c32172f80f4cfa137ba6c7bf35bd622c764643e0

--- src/org/exist/xquery/parser/XQueryLexer.java
+++ src/org/exist/xquery/parser/XQueryLexer.java
@@ -25084,6 +25084,17 @@ operator|-
 name|_begin
 argument_list|)
 decl_stmt|;
+name|System
+operator|.
+name|out
+operator|.
+name|println
+argument_list|(
+literal|"Comment: "
+operator|+
+name|comment
+argument_list|)
+expr_stmt|;
 for|for
 control|(
 name|int
@@ -25113,9 +25124,20 @@ argument_list|)
 operator|==
 literal|'\n'
 condition|)
+block|{
+name|System
+operator|.
+name|out
+operator|.
+name|println
+argument_list|(
+literal|"Found newline"
+argument_list|)
+expr_stmt|;
 name|newline
 argument_list|()
 expr_stmt|;
+block|}
 block|}
 name|_ttype
 operator|=
