ee20a18c5fdf2c039ac1167e87add637d58d316a
Wolfgang Meier <wolfgangmm@gmail.com>
2006-10-15 09:59:14 +0000

    Imported revisions 4610 to 4622 from DLN branch.
    svn path=/trunk/eXist-1.0/; revision=4623
    
    
    Former-commit-id: 000583deb30fc041e23b4890c741aab9b3440a92

--- src/org/exist/xquery/FunctionFactory.java
+++ src/org/exist/xquery/FunctionFactory.java
@@ -1167,7 +1167,7 @@ name|XPathException
 argument_list|(
 name|ast
 argument_list|,
-literal|"Java binding is disabled in the current configuration. Call to "
+literal|"Java binding is disabled in the current configuration (see conf.xml). Call to "
 operator|+
 name|qname
 operator|.
@@ -1187,7 +1187,7 @@ name|XPathException
 argument_list|(
 name|ast
 argument_list|,
-literal|"Java binding is disabled in the current configuration . Call to "
+literal|"Java binding is disabled in the current configuration (see conf.xml). Call to "
 operator|+
 name|qname
 operator|.
