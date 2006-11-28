36a65fbd1ae9270665b8f2d609d22db302494bab
Adam Retter <adam.retter@googlemail.com>
2006-11-28 21:52:54 +0000

    corrected typos in function descriptions
    svn path=/trunk/eXist/; revision=4923
    
    
    Former-commit-id: 39d11c4fb3626a29ebec233725e4a88f53c0055e

--- extensions/modules/src/org/exist/xquery/modules/scheduler/ResumeScheduledJob.java
+++ extensions/modules/src/org/exist/xquery/modules/scheduler/ResumeScheduledJob.java
@@ -227,7 +227,7 @@ operator|.
 name|PREFIX
 argument_list|)
 argument_list|,
-literal|"Resumes the job named in $a with the Scheduler. Will only resume User Scheduled Job's! Returns true() if the Job was deleted."
+literal|"Resumes the job named in $a with the Scheduler. Will only resume User Scheduled Job's!"
 argument_list|,
 operator|new
 name|SequenceType
