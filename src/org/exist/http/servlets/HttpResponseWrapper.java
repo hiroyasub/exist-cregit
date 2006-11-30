a44b0bd3e561fdba766c2c7608e39e4b9ab80588
Leif-Jöran Olsson <ljo@fripost.org>
2006-11-30 16:32:05 +0000

    Added maxAge to response:set-cookie() on suggestion from Golikov. He should supply a test case.
    svn path=/trunk/eXist/; revision=4932
    
    
    Former-commit-id: c504870e2620073d4aad7e6a5486f857e2902a66

--- src/org/exist/http/servlets/HttpResponseWrapper.java
+++ src/org/exist/http/servlets/HttpResponseWrapper.java
@@ -1,6 +1,6 @@
 begin_unit|revision:1.0.0;language:Java;cregit-version:0.0.1
 begin_comment
-comment|/*  *  eXist Open Source Native XML Database  *  Copyright (C) 2001-06 Wolfgang M. Meier  *  wolfgang@exist-db.org  *  http://exist.sourceforge.net  *    *  This program is free software; you can redistribute it and/or  *  modify it under the terms of the GNU Lesser General Public License  *  as published by the Free Software Foundation; either version 2  *  of the License, or (at your option) any later version.  *    *  This program is distributed in the hope that it will be useful,  *  but WITHOUT ANY WARRANTY; without even the implied warranty of  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the  *  GNU Lesser General Public License for more details.  *    *  You should have received a copy of the GNU Lesser General Public License  *  along with this program; if not, write to the Free Software  *  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.  *    *  $Id$  */
+comment|/*  * eXist Open Source Native XML Database  * Copyright (C) 2001-2006 The eXist team  *  http://exist-db.org  *  * This program is free software; you can redistribute it and/or  * modify it under the terms of the GNU Lesser General Public License  * as published by the Free Software Foundation; either version 2  * of the License, or (at your option) any later version.  *    * This program is distributed in the hope that it will be useful,  * but WITHOUT ANY WARRANTY; without even the implied warranty of  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the  * GNU Lesser General Public License for more details.  *   * You should have received a copy of the GNU Lesser General Public License  * along with this program; if not, write to the Free Software Foundation  * Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.  *    *  $Id$  */
 end_comment
 
 begin_package
@@ -142,6 +142,50 @@ name|name
 argument_list|,
 name|value
 argument_list|)
+argument_list|)
+expr_stmt|;
+block|}
+comment|/**      * The method<code>addCookie</code>      *      * @param name Name of the Cookie      * @param value Value of the Cookie      * @param maxAge an<code>int</code> value      */
+specifier|public
+name|void
+name|addCookie
+parameter_list|(
+specifier|final
+name|String
+name|name
+parameter_list|,
+specifier|final
+name|String
+name|value
+parameter_list|,
+specifier|final
+name|int
+name|maxAge
+parameter_list|)
+block|{
+name|Cookie
+name|cookie
+init|=
+operator|new
+name|Cookie
+argument_list|(
+name|name
+argument_list|,
+name|value
+argument_list|)
+decl_stmt|;
+name|cookie
+operator|.
+name|setMaxAge
+argument_list|(
+name|maxAge
+argument_list|)
+expr_stmt|;
+name|response
+operator|.
+name|addCookie
+argument_list|(
+name|cookie
 argument_list|)
 expr_stmt|;
 block|}
