56930ba069970ba68fe9b18b2114bffa1fc751b4
Wolfgang Meier <wolfgangmm@gmail.com>
2006-12-29 18:23:09 +0000

    Performance test suite:
    * added "generate" action to generate XML files using XQuery.
    * new function random-text()
    * added "store" action to store inline XML
    * added "setup" and "tear-down" sections
    * support for test "groups"
    
    svn path=/trunk/eXist/; revision=5095
    
    
    Former-commit-id: c4cba1850a0a117e559aac720104fd7d72988a95

--- test/src/org/exist/performance/actions/Action.java
+++ test/src/org/exist/performance/actions/Action.java
@@ -71,6 +71,16 @@ name|XMLDBException
 import|;
 end_import
 
+begin_import
+import|import
+name|java
+operator|.
+name|io
+operator|.
+name|FileNotFoundException
+import|;
+end_import
+
 begin_comment
 comment|/* *  eXist Open Source Native XML Database *  Copyright (C) 2001-06 The eXist Project *  http://exist-db.org *   *  This program is free software; you can redistribute it and/or *  modify it under the terms of the GNU Lesser General Public License *  as published by the Free Software Foundation; either version 2 *  of the License, or (at your option) any later version. *   *  This program is distributed in the hope that it will be useful, *  but WITHOUT ANY WARRANTY; without even the implied warranty of *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the *  GNU Lesser General Public License for more details. *   *  You should have received a copy of the GNU Lesser General Public License *  along with this program; if not, write to the Free Software *  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA. *   *  $Id$ */
 end_comment
@@ -105,6 +115,8 @@ throws|throws
 name|XMLDBException
 throws|,
 name|EXistException
+throws|,
+name|FileNotFoundException
 function_decl|;
 name|String
 name|getDescription
