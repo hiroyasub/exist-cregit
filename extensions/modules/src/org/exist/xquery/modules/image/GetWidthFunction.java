e0ac6973d8233ee9b5ebde34bc9c925c1e5ad62e
Adam Retter <adam.retter@googlemail.com>
2007-01-16 17:12:33 +0000

    Added a scale() function. get-height() and get-width() now expect image data rather than a db URI. This makes them more adaptable (e.g. can be used with images in the db via. util:binary-doc() or for images embedded in XML documents via. XPath). Also some refactorings for better code sharing
    svn path=/trunk/eXist/; revision=5200
    
    
    Former-commit-id: 924368a1c1fbf66950aae16f37993ff3be463ede

--- extensions/modules/src/org/exist/xquery/modules/image/GetWidthFunction.java
+++ extensions/modules/src/org/exist/xquery/modules/image/GetWidthFunction.java
@@ -33,51 +33,7 @@ name|java
 operator|.
 name|io
 operator|.
-name|ByteArrayInputStream
-import|;
-end_import
-
-begin_import
-import|import
-name|java
-operator|.
-name|io
-operator|.
 name|IOException
-import|;
-end_import
-
-begin_import
-import|import
-name|javax
-operator|.
-name|imageio
-operator|.
-name|ImageIO
-import|;
-end_import
-
-begin_import
-import|import
-name|org
-operator|.
-name|exist
-operator|.
-name|dom
-operator|.
-name|BinaryDocument
-import|;
-end_import
-
-begin_import
-import|import
-name|org
-operator|.
-name|exist
-operator|.
-name|dom
-operator|.
-name|DocumentImpl
 import|;
 end_import
 
@@ -90,44 +46,6 @@ operator|.
 name|dom
 operator|.
 name|QName
-import|;
-end_import
-
-begin_import
-import|import
-name|org
-operator|.
-name|exist
-operator|.
-name|security
-operator|.
-name|PermissionDeniedException
-import|;
-end_import
-
-begin_import
-import|import
-name|org
-operator|.
-name|exist
-operator|.
-name|storage
-operator|.
-name|DBBroker
-import|;
-end_import
-
-begin_import
-import|import
-name|org
-operator|.
-name|exist
-operator|.
-name|storage
-operator|.
-name|lock
-operator|.
-name|Lock
 import|;
 end_import
 
@@ -201,7 +119,7 @@ name|xquery
 operator|.
 name|value
 operator|.
-name|AnyURIValue
+name|Base64Binary
 import|;
 end_import
 
@@ -262,7 +180,7 @@ import|;
 end_import
 
 begin_comment
-comment|/**  * eXist Image Module Extension GetWidthFunction   *   * Get's the width of an Image stored in the eXist db  *   * @author Adam Retter<adam.retter@devon.gov.uk>  * @serial 2006-03-10  * @version 1.0  *  * @see org.exist.xquery.BasicFunction#BasicFunction(org.exist.xquery.XQueryContext, org.exist.xquery.FunctionSignature)  */
+comment|/**  * eXist Image Module Extension GetWidthFunction   *   * Get's the width of an Image  *   * @author Adam Retter<adam.retter@devon.gov.uk>  * @serial 2006-03-10  * @version 1.1  *  * @see org.exist.xquery.BasicFunction#BasicFunction(org.exist.xquery.XQueryContext, org.exist.xquery.FunctionSignature)  */
 end_comment
 
 begin_class
@@ -295,7 +213,7 @@ operator|.
 name|PREFIX
 argument_list|)
 argument_list|,
-literal|"Get's the width of the image in the db indicated by $a, returning an integer of the images width in pixels or an empty sequence if $a is invalid."
+literal|"Get's the Width of the image passed in $a, returning an integer of the images width in pixels or an empty sequence if $a is invalid."
 argument_list|,
 operator|new
 name|SequenceType
@@ -306,7 +224,7 @@ name|SequenceType
 argument_list|(
 name|Type
 operator|.
-name|ANY_URI
+name|BASE64_BINARY
 argument_list|,
 name|Cardinality
 operator|.
@@ -374,12 +292,22 @@ name|Sequence
 operator|.
 name|EMPTY_SEQUENCE
 return|;
-comment|//get the path of the image
-name|AnyURIValue
-name|imgPath
+comment|//get the image
+name|Image
+name|image
 init|=
+literal|null
+decl_stmt|;
+try|try
+block|{
+name|image
+operator|=
+name|ImageModule
+operator|.
+name|getImage
+argument_list|(
 operator|(
-name|AnyURIValue
+name|Base64Binary
 operator|)
 name|args
 index|[
@@ -389,195 +317,6 @@ operator|.
 name|itemAt
 argument_list|(
 literal|0
-argument_list|)
-decl_stmt|;
-comment|//Get the image document from the db
-name|DBBroker
-name|dbbroker
-init|=
-name|context
-operator|.
-name|getBroker
-argument_list|()
-decl_stmt|;
-name|DocumentImpl
-name|docImage
-init|=
-literal|null
-decl_stmt|;
-try|try
-block|{
-name|docImage
-operator|=
-name|dbbroker
-operator|.
-name|getXMLResource
-argument_list|(
-name|imgPath
-operator|.
-name|toXmldbURI
-argument_list|()
-argument_list|,
-name|Lock
-operator|.
-name|READ_LOCK
-argument_list|)
-expr_stmt|;
-block|}
-catch|catch
-parameter_list|(
-name|PermissionDeniedException
-name|e
-parameter_list|)
-block|{
-throw|throw
-operator|new
-name|XPathException
-argument_list|(
-name|getASTNode
-argument_list|()
-argument_list|,
-name|imgPath
-operator|+
-literal|": permission denied to read resource"
-argument_list|)
-throw|;
-block|}
-comment|//Valid document?
-if|if
-condition|(
-name|docImage
-operator|==
-literal|null
-condition|)
-block|{
-name|LOG
-operator|.
-name|error
-argument_list|(
-name|imgPath
-operator|+
-literal|" does not exist!"
-argument_list|)
-expr_stmt|;
-return|return
-name|Sequence
-operator|.
-name|EMPTY_SEQUENCE
-return|;
-block|}
-comment|//Binary Document?
-if|if
-condition|(
-name|docImage
-operator|.
-name|getResourceType
-argument_list|()
-operator|!=
-name|DocumentImpl
-operator|.
-name|BINARY_FILE
-condition|)
-block|{
-name|LOG
-operator|.
-name|error
-argument_list|(
-name|imgPath
-operator|+
-literal|" exists but is not a binary resource!"
-argument_list|)
-expr_stmt|;
-return|return
-name|Sequence
-operator|.
-name|EMPTY_SEQUENCE
-return|;
-block|}
-comment|//Binary document is an image?
-if|if
-condition|(
-operator|!
-name|docImage
-operator|.
-name|getMetadata
-argument_list|()
-operator|.
-name|getMimeType
-argument_list|()
-operator|.
-name|startsWith
-argument_list|(
-literal|"image/"
-argument_list|)
-condition|)
-block|{
-name|LOG
-operator|.
-name|error
-argument_list|(
-name|imgPath
-operator|+
-literal|" exists but is not an image!"
-argument_list|)
-expr_stmt|;
-return|return
-name|Sequence
-operator|.
-name|EMPTY_SEQUENCE
-return|;
-block|}
-comment|//Get the image document as a binary document
-name|BinaryDocument
-name|binImage
-init|=
-operator|(
-name|BinaryDocument
-operator|)
-name|docImage
-decl_stmt|;
-comment|//get a byte array representing the image
-name|byte
-index|[]
-name|imgData
-init|=
-name|dbbroker
-operator|.
-name|getBinaryResource
-argument_list|(
-name|binImage
-argument_list|)
-decl_stmt|;
-comment|//close the image from the db
-name|dbbroker
-operator|.
-name|closeDocument
-argument_list|()
-expr_stmt|;
-name|Image
-name|image
-init|=
-literal|null
-decl_stmt|;
-name|int
-name|iWidth
-init|=
-operator|-
-literal|1
-decl_stmt|;
-comment|//Create an Image object from the byte array
-try|try
-block|{
-name|image
-operator|=
-name|ImageIO
-operator|.
-name|read
-argument_list|(
-operator|new
-name|ByteArrayInputStream
-argument_list|(
-name|imgData
 argument_list|)
 argument_list|)
 expr_stmt|;
@@ -592,9 +331,7 @@ name|LOG
 operator|.
 name|error
 argument_list|(
-name|imgPath
-operator|+
-literal|" could not read image data!"
+literal|"Unabel to read image data!"
 argument_list|)
 expr_stmt|;
 return|return
@@ -604,15 +341,16 @@ name|EMPTY_SEQUENCE
 return|;
 block|}
 comment|//Get the width of the image
+name|int
 name|iWidth
-operator|=
+init|=
 name|image
 operator|.
 name|getWidth
 argument_list|(
 literal|null
 argument_list|)
-expr_stmt|;
+decl_stmt|;
 comment|//did we get the width of the image?
 if|if
 condition|(
@@ -627,9 +365,7 @@ name|LOG
 operator|.
 name|error
 argument_list|(
-name|imgPath
-operator|+
-literal|" could not read image data!"
+literal|"Unable to read image data!"
 argument_list|)
 expr_stmt|;
 return|return
