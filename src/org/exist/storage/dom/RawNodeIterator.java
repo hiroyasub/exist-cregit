3b7c9a5283340192cf2818ab30d633dc102307de
Wolfgang Meier <wolfgangmm@gmail.com>
2007-02-11 19:04:35 +0000

    Build was broken on Java 1.5/1.4
    svn path=/trunk/eXist/; revision=5334
    
    
    Former-commit-id: b3d68bee1221d4aad34b9e5b5b590d1348d46cb7

--- src/org/exist/storage/dom/RawNodeIterator.java
+++ src/org/exist/storage/dom/RawNodeIterator.java
@@ -400,8 +400,6 @@ operator|new
 name|IOException
 argument_list|(
 literal|"Node not found."
-argument_list|,
-name|e
 argument_list|)
 throw|;
 block|}
@@ -448,8 +446,6 @@ name|e
 operator|.
 name|getMessage
 argument_list|()
-argument_list|,
-name|e
 argument_list|)
 throw|;
 block|}
