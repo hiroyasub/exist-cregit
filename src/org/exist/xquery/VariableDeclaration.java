bfb3183f05a5fa8fb301187361cc122969afdaae
Wolfgang Meier <wolfgangmm@gmail.com>
2007-07-01 12:05:25 +0000

    Duplicate global variable declarations should be detected in the XQuery parser, not during query analysis. This fixes [1742697]: enable-query-rewriting="yes" breaks sandbox.xql
    svn path=/trunk/eXist/; revision=6120
    
    
    Former-commit-id: 23b4bcf5279fab0d0a08801d40cd903d8ed8a1f3

--- src/org/exist/xquery/VariableDeclaration.java
+++ src/org/exist/xquery/VariableDeclaration.java
@@ -219,29 +219,10 @@ operator|!=
 literal|null
 condition|)
 block|{
-if|if
-condition|(
-name|myModule
-operator|.
-name|isVarDeclared
-argument_list|(
-name|qn
-argument_list|)
-condition|)
-throw|throw
-operator|new
-name|XPathException
-argument_list|(
-name|getASTNode
-argument_list|()
-argument_list|,
-literal|"err:XQST0049: It is a static error if more than one "
-operator|+
-literal|"variable declared or imported by a module has the same expanded QName. Variable: "
-operator|+
-name|qn
-argument_list|)
-throw|;
+comment|// WM: duplicate var declaration is now caught in the XQuery tree parser
+comment|//            if (myModule.isVarDeclared(qn))
+comment|//                throw new XPathException(getASTNode(), "err:XQST0049: It is a static error if more than one " +
+comment|//                    "variable declared or imported by a module has the same expanded QName. Variable: " + qn);
 name|myModule
 operator|.
 name|declareVariable
@@ -252,31 +233,11 @@ expr_stmt|;
 block|}
 else|else
 block|{
-if|if
-condition|(
-name|context
-operator|.
-name|isVarDeclared
-argument_list|(
-name|qn
-argument_list|)
-condition|)
-block|{
-throw|throw
-operator|new
-name|XPathException
-argument_list|(
-name|getASTNode
-argument_list|()
-argument_list|,
-literal|"err:XQST0049: It is a static error if more than one "
-operator|+
-literal|"variable declared or imported by a module has the same expanded QName. Variable: "
-operator|+
-name|qn
-argument_list|)
-throw|;
-block|}
+comment|// WM: duplicate var declaration is now caught in the XQuery tree parser
+comment|//            if(context.isVarDeclared(qn)) {
+comment|//                throw new XPathException(getASTNode(), "err:XQST0049: It is a static error if more than one " +
+comment|//                        "variable declared or imported by a module has the same expanded QName. Variable: " + qn);
+comment|//            }
 name|context
 operator|.
 name|declareGlobalVariable
