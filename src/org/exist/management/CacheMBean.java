49eaf05f2dd3b93f5d3e4ea62e02b39316492b6f
Wolfgang Meier <wolfgangmm@gmail.com>
2007-07-08 14:20:05 +0000

    * Improved calculation of free memory during indexing: to avoid out-of-memory issues, eXist should at least reserve enough memory to hold the page and collection caches.
    * Added MBean to export some basic information about the database instance, cache sizes and reserved memory.
    
    svn path=/trunk/eXist/; revision=6175
    
    
    Former-commit-id: f99ad18ef2a8ae4ca19468cd283a2b88487b38e0

--- src/org/exist/management/CacheMBean.java
+++ src/org/exist/management/CacheMBean.java
@@ -1,4 +1,8 @@
 begin_unit|revision:1.0.0;language:Java;cregit-version:0.0.1
+begin_comment
+comment|/*  *  eXist Open Source Native XML Database  *  Copyright (C) 2001-07 The eXist Project  *  http://exist-db.org  *  *  This program is free software; you can redistribute it and/or  *  modify it under the terms of the GNU Lesser General Public License  *  as published by the Free Software Foundation; either version 2  *  of the License, or (at your option) any later version.  *  *  This program is distributed in the hope that it will be useful,  *  but WITHOUT ANY WARRANTY; without even the implied warranty of  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the  *  GNU Lesser General Public License for more details.  *  *  You should have received a copy of the GNU Lesser General Public  *  License along with this library; if not, write to the Free Software  *  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA  *  * $Id$  */
+end_comment
+
 begin_package
 package|package
 name|org
@@ -10,7 +14,7 @@ package|;
 end_package
 
 begin_comment
-comment|/**  * Created by IntelliJ IDEA.  * User: wolf  * Date: Jun 9, 2007  * Time: 8:51:10 PM  * To change this template use File | Settings | File Templates.  */
+comment|/**  * Provides access to some properties of the internal page caches  * ({@link org.exist.storage.cache.Cache}).  */
 end_comment
 
 begin_interface
