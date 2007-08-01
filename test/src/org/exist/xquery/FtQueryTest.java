5b4b1de8a733220cf4935dc9f9079ade4538751c
Leif-Jöran Olsson <ljo@fripost.org>
2007-08-01 18:16:38 +0000

    Header fixes. Hilite matches off diff for fttest
    svn path=/trunk/eXist/; revision=6322
    
    
    Former-commit-id: 228bda6a85af5a47561a5186bf1c1dd946baa626

--- test/src/org/exist/xquery/FtQueryTest.java
+++ test/src/org/exist/xquery/FtQueryTest.java
@@ -1,6 +1,6 @@
 begin_unit|revision:1.0.0;language:Java;cregit-version:0.0.1
 begin_comment
-comment|/*  *  eXist Open Source Native XML Database  *  Copyright (C) 2001-04 The eXist Project  *  http://exist-db.org  *    *  This program is free software; you can redistribute it and/or  *  modify it under the terms of the GNU Lesser General Public License  *  as published by the Free Software Foundation; either version 2  *  of the License, or (at your option) any later version.  *    *  This program is distributed in the hope that it will be useful,  *  but WITHOUT ANY WARRANTY; without even the implied warranty of  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the  *  GNU Lesser General Public License for more details.  *    *  You should have received a copy of the GNU Lesser General Public License  *  along with this program; if not, write to the Free Software  *  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.  *    *  $Id$  */
+comment|/*  * eXist Open Source Native XML Database  * Copyright (C) 2001-2007 The eXist Project  * http://exist-db.org  *  * This program is free software; you can redistribute it and/or  * modify it under the terms of the GNU Lesser General Public License  * as published by the Free Software Foundation; either version 2  * of the License, or (at your option) any later version.  *    * This program is distributed in the hope that it will be useful,  * but WITHOUT ANY WARRANTY; without even the implied warranty of  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the  * GNU Lesser General Public License for more details.  *   * You should have received a copy of the GNU Lesser General Public License  * along with this program; if not, write to the Free Software Foundation  * Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.  *    *  $Id$  */
 end_comment
 
 begin_package
@@ -530,6 +530,7 @@ argument_list|,
 literal|"<LINE>Fillet of a fenny snake,</LINE>"
 argument_list|)
 expr_stmt|;
+comment|//assertXMLEqual(result.getResource(0).getContent().toString(), "<LINE>Fillet of a<exist:match xmlns:exist='http://exist.sourceforge.net/NS/exist'>fenny</exist:match><exist:match xmlns:exist='http://exist.sourceforge.net/NS/exist'>snake</exist:match>,</LINE>");
 name|result
 operator|=
 name|service
@@ -855,6 +856,7 @@ argument_list|,
 literal|"<LINE>Fillet of a fenny snake,</LINE>"
 argument_list|)
 expr_stmt|;
+comment|//assertXMLEqual(result.getResource(0).getContent().toString(), "<LINE>Fillet of a<exist:match xmlns:exist='http://exist.sourceforge.net/NS/exist'>fenny</exist:match><exist:match xmlns:exist='http://exist.sourceforge.net/NS/exist'>snake</exist:match>,</LINE>");
 name|result
 operator|=
 name|service
@@ -892,6 +894,7 @@ argument_list|,
 literal|"<LINE>Fillet of a fenny snake,</LINE>"
 argument_list|)
 expr_stmt|;
+comment|//assertXMLEqual(result.getResource(0).getContent().toString(), "<LINE>Fillet of a<exist:match xmlns:exist='http://exist.sourceforge.net/NS/exist'>fenny</exist:match><exist:match xmlns:exist='http://exist.sourceforge.net/NS/exist'>snake</exist:match>,</LINE>");
 name|result
 operator|=
 name|service
