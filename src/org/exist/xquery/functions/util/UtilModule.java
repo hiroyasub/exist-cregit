80d25caa0c2f608bb5d03fe51995d0fa9e329a0c
Pierrick Brihaye <pierrick.brihaye@free.fr>
2007-09-12 13:46:50 +0000

    Introduced a new util:get-sequence-type() function that will be needed by the XQTS app. I hope we won't have the same never-ending discussions about the accuracy of such a function as there were on the XML mailing-lists :-) Revised string representation of some types. Might be discussed though...
    svn path=/trunk/eXist/; revision=6541
    
    
    Former-commit-id: 25699bae1f3cfeced4f66c6eaa48b109cc9f23ff

--- src/org/exist/xquery/functions/util/UtilModule.java
+++ src/org/exist/xquery/functions/util/UtilModule.java
@@ -879,6 +879,18 @@ operator|.
 name|class
 argument_list|)
 block|,
+operator|new
+name|FunctionDef
+argument_list|(
+name|GetSequenceType
+operator|.
+name|signature
+argument_list|,
+name|GetSequenceType
+operator|.
+name|class
+argument_list|)
+block|,
 comment|// deprecated functions
 operator|new
 name|FunctionDef
