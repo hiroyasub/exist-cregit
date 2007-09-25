0702d50d463bd574865dffbf4e814d8be9b32d9e
Wolfgang Meier <wolfgangmm@gmail.com>
2007-09-25 13:50:20 +0000

    Added function util:parse(xs:string?) to parse a string value into an XML fragment. Previously, util:eval had to be used for this purpose, but it added unnecessary overhead and did not work for all cases.
    svn path=/trunk/eXist/; revision=6626
    
    
    Former-commit-id: 5966dcb4e210ccc502d9a825a8bffa55850a1c93

--- src/org/exist/xquery/functions/util/UtilModule.java
+++ src/org/exist/xquery/functions/util/UtilModule.java
@@ -891,6 +891,18 @@ operator|.
 name|class
 argument_list|)
 block|,
+operator|new
+name|FunctionDef
+argument_list|(
+name|Parse
+operator|.
+name|signature
+argument_list|,
+name|Parse
+operator|.
+name|class
+argument_list|)
+block|,
 comment|// deprecated functions
 operator|new
 name|FunctionDef
