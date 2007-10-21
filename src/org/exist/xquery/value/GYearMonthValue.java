f38d5dbfee519dfac2ad7f7017dbdb4ecad31048
Leif-Jöran Olsson <ljo@fripost.org>
2007-10-21 22:17:55 +0000

    Two more xqts fixes, K-SeqExprCast-331 and K-SeqExprCast-394. Probably a few more of this possible without redesign.
    svn path=/trunk/eXist/; revision=6767
    
    
    Former-commit-id: bc9833714ca5537ee8d895d6932dc84959e9b948

--- src/org/exist/xquery/value/GYearMonthValue.java
+++ src/org/exist/xquery/value/GYearMonthValue.java
@@ -1,6 +1,6 @@
 begin_unit|revision:1.0.0;language:Java;cregit-version:0.0.1
 begin_comment
-comment|/*  *  eXist Open Source Native XML Database  *  Copyright (C) 2001-04 The eXist Project  *  http://exist-db.org  *    *  This program is free software; you can redistribute it and/or  *  modify it under the terms of the GNU Lesser General Public License  *  as published by the Free Software Foundation; either version 2  *  of the License, or (at your option) any later version.  *    *  This program is distributed in the hope that it will be useful,  *  but WITHOUT ANY WARRANTY; without even the implied warranty of  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the  *  GNU Lesser General Public License for more details.  *    *  You should have received a copy of the GNU Lesser General Public License  *  along with this program; if not, write to the Free Software  *  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.  *    *  $Id$  */
+comment|/*  * eXist Open Source Native XML Database  * Copyright (C) 2001-2007 The eXist Project  * http://exist-db.org  *  * This program is free software; you can redistribute it and/or  * modify it under the terms of the GNU Lesser General Public License  * as published by the Free Software Foundation; either version 2  * of the License, or (at your option) any later version.  *    * This program is distributed in the hope that it will be useful,  * but WITHOUT ANY WARRANTY; without even the implied warranty of  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the  * GNU Lesser General Public License for more details.  *   * You should have received a copy of the GNU Lesser General Public License  * along with this program; if not, write to the Free Software Foundation  * Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.  *    *  $Id$  */
 end_comment
 
 begin_package
@@ -492,6 +492,34 @@ return|;
 block|}
 else|else
 block|{
+comment|// equal? Is this sufficient? /ljo
+if|if
+condition|(
+name|this
+operator|.
+name|getTrimmedCalendar
+argument_list|()
+operator|.
+name|compare
+argument_list|(
+operator|(
+operator|(
+name|AbstractDateTimeValue
+operator|)
+name|other
+operator|)
+operator|.
+name|getTrimmedCalendar
+argument_list|()
+argument_list|)
+operator|==
+literal|0
+condition|)
+return|return
+name|DatatypeConstants
+operator|.
+name|EQUAL
+return|;
 if|if
 condition|(
 operator|!
