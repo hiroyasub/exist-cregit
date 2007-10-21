0c42ffcb39de2c49ccbb43edaa8ff3257108b266
Leif-Jöran Olsson <ljo@fripost.org>
2007-10-21 19:53:05 +0000

    One xqts fix, CastFOCA0003-1, and some header changes.
    svn path=/trunk/eXist/; revision=6766
    
    
    Former-commit-id: cfcc3e73ca7f5cfc70c2e9ad45d35a042f8add31

--- src/org/exist/xquery/value/DoubleValue.java
+++ src/org/exist/xquery/value/DoubleValue.java
@@ -1,6 +1,6 @@
 begin_unit|revision:1.0.0;language:Java;cregit-version:0.0.1
 begin_comment
-comment|/*  *  eXist Open Source Native XML Database  *  Copyright (C) 2001-06,  Wolfgang M. Meier (meier@ifs.tu-darmstadt.de)  *  *  This library is free software; you can redistribute it and/or  *  modify it under the terms of the GNU Library General Public License  *  as published by the Free Software Foundation; either version 2  *  of the License, or (at your option) any later version.  *  *  This library is distributed in the hope that it will be useful,  *  but WITHOUT ANY WARRANTY; without even the implied warranty of  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the  *  GNU Library General Public License for more details.  *  *  You should have received a copy of the GNU Library General Public License  *  along with this program; if not, write to the Free Software  *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.  *   *  $Id$  */
+comment|/*  * eXist Open Source Native XML Database  * Copyright (C) 2001-2007 The eXist Project  * http://exist-db.org  *  * This program is free software; you can redistribute it and/or  * modify it under the terms of the GNU Lesser General Public License  * as published by the Free Software Foundation; either version 2  * of the License, or (at your option) any later version.  *    * This program is distributed in the hope that it will be useful,  * but WITHOUT ANY WARRANTY; without even the implied warranty of  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the  * GNU Lesser General Public License for more details.  *   * You should have received a copy of the GNU Lesser General Public License  * along with this program; if not, write to the Free Software Foundation  * Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.  *    *  $Id$  */
 end_comment
 
 begin_package
@@ -759,6 +759,21 @@ name|getTypeName
 argument_list|(
 name|requiredType
 argument_list|)
+argument_list|)
+throw|;
+if|if
+condition|(
+name|value
+operator|>
+name|Integer
+operator|.
+name|MAX_VALUE
+condition|)
+throw|throw
+operator|new
+name|XPathException
+argument_list|(
+literal|"err:FOCA0003: Value is out of range for type xs:integer"
 argument_list|)
 throw|;
 return|return
