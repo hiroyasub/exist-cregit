ee4e6b1dd02ce375042fcea7d6af9c7fc85905fb
Leif-Jöran Olsson <ljo@fripost.org>
2007-10-24 20:03:10 +0000

    Some more XQTS-fixes, just one remaining test failure in Expressions on sequence types.
    svn path=/trunk/eXist/; revision=6783
    
    
    Former-commit-id: d1b4586f1a4d310dd3137287cfd8c2bc5af9c652

--- src/org/exist/xquery/CastExpression.java
+++ src/org/exist/xquery/CastExpression.java
@@ -1,6 +1,6 @@
 begin_unit|revision:1.0.0;language:Java;cregit-version:0.0.1
 begin_comment
-comment|/*  *  eXist Open Source Native XML Database  *  Copyright (C) 2001-06 Wolfgang M. Meier  *  wolfgang@exist-db.org  *  http://exist.sourceforge.net  *    *  This program is free software; you can redistribute it and/or  *  modify it under the terms of the GNU Lesser General Public License  *  as published by the Free Software Foundation; either version 2  *  of the License, or (at your option) any later version.  *    *  This program is distributed in the hope that it will be useful,  *  but WITHOUT ANY WARRANTY; without even the implied warranty of  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the  *  GNU Lesser General Public License for more details.  *    *  You should have received a copy of the GNU Lesser General Public License  *  along with this program; if not, write to the Free Software  *  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.  *    *  $Id$  */
+comment|/*  * eXist Open Source Native XML Database  * Copyright (C) 2001-2007 The eXist Project  * http://exist-db.org  *  * This program is free software; you can redistribute it and/or  * modify it under the terms of the GNU Lesser General Public License  * as published by the Free Software Foundation; either version 2  * of the License, or (at your option) any later version.  *    * This program is distributed in the hope that it will be useful,  * but WITHOUT ANY WARRANTY; without even the implied warranty of  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the  * GNU Lesser General Public License for more details.  *   * You should have received a copy of the GNU Lesser General Public License  * along with this program; if not, write to the Free Software Foundation  * Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.  *    *  $Id$  */
 end_comment
 
 begin_package
@@ -430,6 +430,34 @@ argument_list|(
 literal|0
 argument_list|)
 decl_stmt|;
+if|if
+condition|(
+name|seq
+operator|.
+name|hasMany
+argument_list|()
+operator|&&
+name|Type
+operator|.
+name|subTypeOf
+argument_list|(
+name|requiredType
+argument_list|,
+name|Type
+operator|.
+name|ATOMIC
+argument_list|)
+condition|)
+throw|throw
+operator|new
+name|XPathException
+argument_list|(
+name|getASTNode
+argument_list|()
+argument_list|,
+literal|"err:XPTY0004: cardinality error: sequence with more than one item is not allowed here"
+argument_list|)
+throw|;
 try|try
 block|{
 comment|// casting to QName needs special treatment
