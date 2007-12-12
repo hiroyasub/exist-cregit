2d86428dd44805b4dc5ee520fac895f5fad736ae
Adam Retter <adam.retter@googlemail.com>
2007-12-12 12:24:25 +0000

    new functions util:node-xpath() gives you the xpath of a node, attributes in the hierarchy are converted to predicates
    svn path=/trunk/eXist/; revision=7058
    
    
    Former-commit-id: 6d65964a2d1db9f316d888291c2eef77900506cb

--- src/org/exist/xquery/functions/util/UtilModule.java
+++ src/org/exist/xquery/functions/util/UtilModule.java
@@ -963,6 +963,18 @@ operator|.
 name|class
 argument_list|)
 block|,
+operator|new
+name|FunctionDef
+argument_list|(
+name|NodeXPath
+operator|.
+name|signature
+argument_list|,
+name|NodeXPath
+operator|.
+name|class
+argument_list|)
+block|,
 comment|// deprecated functions
 operator|new
 name|FunctionDef
