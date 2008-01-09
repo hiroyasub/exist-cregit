ec0aa127e32f7e2e6c5ada5aafe8cba2a58d7825
Leif-Jöran Olsson <ljo@fripost.org>
2008-01-09 23:00:42 +0000

    Return the PUTted Document with GET to show as result.
    svn path=/trunk/eXist/; revision=7170
    
    
    Former-commit-id: 29dd33552eacd3b75ee4e6b8d9b6469168a8a495

--- samples/src/org/exist/examples/http/PutExample.java
+++ samples/src/org/exist/examples/http/PutExample.java
@@ -1,6 +1,6 @@
 begin_unit|revision:1.0.0;language:Java;cregit-version:0.0.1
 begin_comment
-comment|/*  *  eXist Open Source Native XML Database  *  Copyright (C) 2001-08 Wolfgang M. Meier  *  wolfgang@exist-db.org  *  http://exist-db.org  *  *  This program is free software; you can redistribute it and/or  *  modify it under the terms of the GNU Lesser General Public License  *  as published by the Free Software Foundation; either version 2  *  of the License, or (at your option) any later version.  *  *  This program is distributed in the hope that it will be useful,  *  but WITHOUT ANY WARRANTY; without even the implied warranty of  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the  *  GNU Lesser General Public License for more details.  *  *  You should have received a copy of the GNU Lesser General Public License  *  along with this program; if not, write to the Free Software  *  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.  *   *  $Id$  */
+comment|/*  * eXist Open Source Native XML Database  * Copyright (C) 2001-2008 The eXist Project  * http://exist-db.org  *  * This program is free software; you can redistribute it and/or  * modify it under the terms of the GNU Lesser General Public License  * as published by the Free Software Foundation; either version 2  * of the License, or (at your option) any later version.  *    * This program is distributed in the hope that it will be useful,  * but WITHOUT ANY WARRANTY; without even the implied warranty of  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the  * GNU Lesser General Public License for more details.  *   * You should have received a copy of the GNU Lesser General Public License  * along with this program; if not, write to the Free Software Foundation  * Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.  *    *  $Id$  */
 end_comment
 
 begin_package
@@ -108,7 +108,7 @@ import|;
 end_import
 
 begin_comment
-comment|/**  * PutExample  * Execute: bin\run.bat org.exist.examples.http.PutExample  *   * @author wolf  *  */
+comment|/**  * PutExample  * Execute: bin\run.bat org.exist.examples.http.PutExample<fileid>  * Make sure you have the server started with bin\startup.bat beforehand.  *  * @author wolf  * @author ljo  *  */
 end_comment
 
 begin_class
@@ -141,7 +141,7 @@ name|out
 operator|.
 name|println
 argument_list|(
-literal|"Usage: org.exist.examples.http.PutExample<file>"
+literal|"Usage: bin/run.sh org.exist.examples.http.PutExample<fileid>"
 argument_list|)
 expr_stmt|;
 name|System
@@ -207,7 +207,7 @@ init|=
 operator|new
 name|URL
 argument_list|(
-literal|"http://admin:@localhost:8080/exist/rest"
+literal|"http://localhost:8080/exist/rest"
 operator|+
 name|DBBroker
 operator|.
@@ -321,6 +321,28 @@ name|url
 operator|.
 name|toString
 argument_list|()
+argument_list|)
+expr_stmt|;
+name|connect
+operator|.
+name|connect
+argument_list|()
+expr_stmt|;
+name|connect
+operator|=
+operator|(
+name|HttpURLConnection
+operator|)
+name|url
+operator|.
+name|openConnection
+argument_list|()
+expr_stmt|;
+name|connect
+operator|.
+name|setRequestMethod
+argument_list|(
+literal|"GET"
 argument_list|)
 expr_stmt|;
 name|connect
