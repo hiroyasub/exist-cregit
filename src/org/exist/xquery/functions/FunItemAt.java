4f142a3ca7d181f5e8a86866eb978bdd1fc6e91a
Pierrick Brihaye <pierrick.brihaye@free.fr>
2008-04-02 08:18:46 +0000

    [bugfix] actually deprecated the legacy function fn:item-at().
    svn path=/trunk/eXist/; revision=7588
    
    
    Former-commit-id: 6be1da67db8780697f9fee3d93d8e08adc086465

--- src/org/exist/xquery/functions/FunItemAt.java
+++ src/org/exist/xquery/functions/FunItemAt.java
@@ -213,7 +213,7 @@ argument_list|)
 argument_list|,
 literal|"Returns the item in the first argument sequence that is located at the position "
 operator|+
-literal|"specified by the second argument. Deprecated. Use $x[1] instead."
+literal|"specified by the second argument."
 argument_list|,
 operator|new
 name|SequenceType
@@ -255,6 +255,8 @@ name|Cardinality
 operator|.
 name|ZERO_OR_ONE
 argument_list|)
+argument_list|,
+literal|"Use $x[1] instead"
 argument_list|)
 decl_stmt|;
 specifier|public
