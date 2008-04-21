e644b459209c6f3ae8de0ec79215a641cd0bc107
Dannes Wessels <dannes@exist-db.org>
2008-04-21 18:48:32 +0000

    [bugfix] cached queries were removed from pool although timeout-check-interval="-1" was set.
    svn path=/trunk/eXist/; revision=7646
    
    
    Former-commit-id: d0cefd84c530587821e8a0adfee2f2a46078023e

--- src/org/exist/storage/XQueryPool.java
+++ src/org/exist/storage/XQueryPool.java
@@ -758,6 +758,13 @@ argument_list|()
 decl_stmt|;
 if|if
 condition|(
+name|timeoutCheckInterval
+operator|<
+literal|0
+condition|)
+return|return;
+if|if
+condition|(
 name|currentTime
 operator|-
 name|lastTimeOutCheck
