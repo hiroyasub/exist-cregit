4082f88da0a799f793a2c4cca9f545e8180c9c0a
Wolfgang Meier <wolfgangmm@gmail.com>
2008-05-07 20:43:07 +0000

    [performance] fixed performance problem with path expression on large node set containing nested nodes, e.g. $n/ancestor::*[@attr]
    svn path=/trunk/eXist/; revision=7723
    
    
    Former-commit-id: a18f704db755b11f157dd21ce531b4c5919be133

--- src/org/exist/xquery/LocationStep.java
+++ src/org/exist/xquery/LocationStep.java
@@ -2428,6 +2428,12 @@ condition|(
 name|contextSet
 operator|instanceof
 name|ExtArrayNodeSet
+operator|&&
+operator|!
+name|contextSet
+operator|.
+name|getProcessInReverseOrder
+argument_list|()
 condition|)
 block|{
 return|return
@@ -2838,6 +2844,12 @@ condition|(
 name|contextSet
 operator|instanceof
 name|ExtArrayNodeSet
+operator|&&
+operator|!
+name|contextSet
+operator|.
+name|getProcessInReverseOrder
+argument_list|()
 condition|)
 block|{
 return|return
