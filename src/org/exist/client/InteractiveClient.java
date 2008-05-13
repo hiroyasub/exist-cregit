5149d9300e382f5ade3380b66454811d7d1598ff
Dannes Wessels <dannes@exist-db.org>
2008-05-13 20:25:05 +0000

    [ignore] we live in 2008 right now... fixed System.out.println()'s
    svn path=/trunk/eXist/; revision=7761
    
    
    Former-commit-id: ccbd1c35db7428dce8019a5317fc1651d3eec729

--- src/org/exist/client/InteractiveClient.java
+++ src/org/exist/client/InteractiveClient.java
@@ -13625,7 +13625,7 @@ argument_list|(
 literal|"product-version"
 argument_list|)
 operator|+
-literal|", Copyright (C) 2001-2007 Wolfgang Meier"
+literal|", Copyright (C) 2001-2008 Wolfgang Meier"
 argument_list|)
 expr_stmt|;
 name|messageln
