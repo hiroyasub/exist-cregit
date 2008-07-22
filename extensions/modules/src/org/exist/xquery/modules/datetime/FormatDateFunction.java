371502340a03f3575022834de708a79cd12c4fdc
Leif-Jöran Olsson <ljo@fripost.org>
2008-07-22 19:30:46 +0000

    [doc] Added SimpleDateFormat hint for template to datetime:formate-date() description.
    svn path=/trunk/eXist/; revision=8051
    
    
    Former-commit-id: e69f43463d4b159344b6105b2cf40c728d11fc0c

--- extensions/modules/src/org/exist/xquery/modules/datetime/FormatDateFunction.java
+++ extensions/modules/src/org/exist/xquery/modules/datetime/FormatDateFunction.java
@@ -1,6 +1,6 @@
 begin_unit|revision:1.0.0;language:Java;cregit-version:0.0.1
 begin_comment
-comment|/*  *  eXist Open Source Native XML Database  *  Copyright (C) 2001-03 Wolfgang M. Meier  *  wolfgang@exist-db.org  *  http://exist.sourceforge.net  *    *  This program is free software; you can redistribute it and/or  *  modify it under the terms of the GNU Lesser General Public License  *  as published by the Free Software Foundation; either version 2  *  of the License, or (at your option) any later version.  *    *  This program is distributed in the hope that it will be useful,  *  but WITHOUT ANY WARRANTY; without even the implied warranty of  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the  *  GNU Lesser General Public License for more details.  *    *  You should have received a copy of the GNU Lesser General Public License  *  along with this program; if not, write to the Free Software  *  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.  *    *  $Id: EchoFunction.java 3063 2006-04-05 20:49:44Z brihaye $  */
+comment|/*  * eXist Open Source Native XML Database  * Copyright (C) 2006-2008 The eXist Project  * http://exist-db.org  *  * This program is free software; you can redistribute it and/or  * modify it under the terms of the GNU Lesser General Public License  * as published by the Free Software Foundation; either version 2  * of the License, or (at your option) any later version.  *    * This program is distributed in the hope that it will be useful,  * but WITHOUT ANY WARRANTY; without even the implied warranty of  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the  * GNU Lesser General Public License for more details.  *   * You should have received a copy of the GNU Lesser General Public License  * along with this program; if not, write to the Free Software Foundation  * Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.  *    *  $Id$  */
 end_comment
 
 begin_package
@@ -213,7 +213,7 @@ operator|.
 name|PREFIX
 argument_list|)
 argument_list|,
-literal|"Returns a xs:string of the xs:date in $a formatted according to the specification in $b"
+literal|"Returns a xs:string of the xs:date in $a formatted according to the template specification in $b as in java.text.SimpleDateFormat."
 argument_list|,
 operator|new
 name|SequenceType
