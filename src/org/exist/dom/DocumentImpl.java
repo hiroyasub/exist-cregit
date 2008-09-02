3b0bd098c4a406d6dcadddc6a9340db1bef98d72
Wolfgang Meier <wolfgangmm@gmail.com>
2008-09-02 13:18:10 +0000

    [ignore] make sure a stack trace is printed if a lock issue is detected
    svn path=/trunk/eXist/; revision=8138
    
    
    Former-commit-id: 38899dedcfced443702ddd20fc2b36ec250db379

--- src/org/exist/dom/DocumentImpl.java
+++ src/org/exist/dom/DocumentImpl.java
@@ -1414,6 +1414,10 @@ operator|.
 name|warn
 argument_list|(
 literal|"document not locked for write !"
+argument_list|,
+operator|new
+name|Throwable
+argument_list|()
 argument_list|)
 expr_stmt|;
 block|}
