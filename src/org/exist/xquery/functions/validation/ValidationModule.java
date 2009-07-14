82cea7e307561cb3b63f5510e51e784d0dbc4cfb
Dannes Wessels <dannes@exist-db.org>
2009-07-14 12:49:54 +0000

    [ignore] updated validation feature. JAXV seems to be complete, SCH support by JING complains of missing schemas
    svn path=/trunk/eXist/; revision=9351
    
    
    Former-commit-id: 0654d307a65850df8b002bb8d0ce9f6efa8e833c

--- src/org/exist/xquery/functions/validation/ValidationModule.java
+++ src/org/exist/xquery/functions/validation/ValidationModule.java
@@ -1,6 +1,6 @@
 begin_unit|revision:1.0.0;language:Java;cregit-version:0.0.1
 begin_comment
-comment|/*  *  eXist Open Source Native XML Database  *  Copyright (C) 2001-07 The eXist Project  *  http://exist-db.org  *  *  This program is free software; you can redistribute it and/or  *  modify it under the terms of the GNU Lesser General Public License  *  as published by the Free Software Foundation; either version 2  *  of the License, or (at your option) any later version.  *  *  This program is distributed in the hope that it will be useful,  *  but WITHOUT ANY WARRANTY; without even the implied warranty of  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the  *  GNU Lesser General Public License for more details.  *  *  You should have received a copy of the GNU Lesser General Public  *  License along with this library; if not, write to the Free Software  *  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA  *  *  $Id$  */
+comment|/*  *  eXist Open Source Native XML Database  *  Copyright (C) 2009 The eXist Project  *  http://exist-db.org  *  *  This program is free software; you can redistribute it and/or  *  modify it under the terms of the GNU Lesser General Public License  *  as published by the Free Software Foundation; either version 2  *  of the License, or (at your option) any later version.  *  *  This program is distributed in the hope that it will be useful,  *  but WITHOUT ANY WARRANTY; without even the implied warranty of  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the  *  GNU Lesser General Public License for more details.  *  *  You should have received a copy of the GNU Lesser General Public  *  License along with this library; if not, write to the Free Software  *  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA  *  *  $Id$  */
 end_comment
 
 begin_package
@@ -207,8 +207,8 @@ argument_list|)
 block|,
 comment|//       new FunctionDef(Jaxv.signatures[0], Jaxv.class),
 comment|//       new FunctionDef(Jaxv.signatures[1], Jaxv.class),
-comment|//        new FunctionDef(Jing.signatures[0], Jing.class),
-comment|//        new FunctionDef(Jing.signatures[1], Jing.class),
+comment|//       new FunctionDef(Jing.signatures[0], Jing.class),
+comment|//       new FunctionDef(Jing.signatures[1], Jing.class),
 block|}
 decl_stmt|;
 specifier|public
