fe987f60dfe31938a6caf97b5968a76bf44ab9ce
Loren Cahlander <loren.cahlander@gmail.com>
2009-07-24 02:46:28 +0000

    [documentation] enhancing function documentation
    svn path=/trunk/eXist/; revision=9479
    
    
    Former-commit-id: f94120633ddcbb720b4862699ab3bc4b4ce54e83

--- src/org/exist/xquery/value/FunctionReturnSequenceType.java
+++ src/org/exist/xquery/value/FunctionReturnSequenceType.java
@@ -22,23 +22,20 @@ end_comment
 begin_class
 specifier|public
 class|class
-name|FunctionParameterSequenceType
-extends|extends
 name|FunctionReturnSequenceType
+extends|extends
+name|SequenceType
 block|{
 specifier|private
 name|String
-name|attributeName
+name|description
 init|=
 literal|null
 decl_stmt|;
-comment|/** 	 * @param attributeName	The name of the parameter in the<strong>FunctionSignature</strong>. 	 * @param primaryType	The<strong>Type</strong> of the parameter. 	 * @param cardinality	The<strong>Cardinality</strong> of the parameter. 	 * @param description	A description of the parameter in the<strong>FunctionSignature</strong>. 	 * @see FunctionSignature, Type, Cardinality 	 */
+comment|/** 	 * @param primaryType	The<strong>Type</strong> of the parameter. 	 * @param cardinality	The<strong>Cardinality</strong> of the parameter. 	 * @param description	A description of the parameter in the<strong>FunctionSignature</strong>. 	 * @see FunctionSignature, Type, Cardinality 	 */
 specifier|public
-name|FunctionParameterSequenceType
+name|FunctionReturnSequenceType
 parameter_list|(
-name|String
-name|attributeName
-parameter_list|,
 name|int
 name|primaryType
 parameter_list|,
@@ -54,41 +51,39 @@ argument_list|(
 name|primaryType
 argument_list|,
 name|cardinality
-argument_list|,
-name|description
 argument_list|)
 expr_stmt|;
 name|this
 operator|.
-name|attributeName
+name|description
 operator|=
-name|attributeName
+name|description
 expr_stmt|;
 block|}
-comment|/** 	 * @return the attributeName 	 */
+comment|/** 	 * @return the description 	 */
 specifier|public
 name|String
-name|getAttributeName
+name|getDescription
 parameter_list|()
 block|{
 return|return
-name|attributeName
+name|description
 return|;
 block|}
-comment|/** 	 * @param attributeName the attributeName to set 	 */
+comment|/** 	 * @param description the description to set 	 */
 specifier|public
 name|void
-name|setAttributeName
+name|setDescription
 parameter_list|(
 name|String
-name|attributeName
+name|description
 parameter_list|)
 block|{
 name|this
 operator|.
-name|attributeName
+name|description
 operator|=
-name|attributeName
+name|description
 expr_stmt|;
 block|}
 block|}
