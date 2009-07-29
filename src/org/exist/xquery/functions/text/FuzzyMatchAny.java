2cc3e79f7ef2ba9cfa49d80569e1a59669903fa3
Loren Cahlander <loren.cahlander@gmail.com>
2009-07-29 04:06:25 +0000

    [documentation] enhancing function module documentation.
    svn path=/trunk/eXist/; revision=9568
    
    
    Former-commit-id: a27ac396f8f47af797b53f7d80c7cbcaba0d6b5e

--- src/org/exist/xquery/functions/text/FuzzyMatchAny.java
+++ src/org/exist/xquery/functions/text/FuzzyMatchAny.java
@@ -87,6 +87,34 @@ name|xquery
 operator|.
 name|value
 operator|.
+name|FunctionParameterSequenceType
+import|;
+end_import
+
+begin_import
+import|import
+name|org
+operator|.
+name|exist
+operator|.
+name|xquery
+operator|.
+name|value
+operator|.
+name|FunctionReturnSequenceType
+import|;
+end_import
+
+begin_import
+import|import
+name|org
+operator|.
+name|exist
+operator|.
+name|xquery
+operator|.
+name|value
+operator|.
 name|SequenceType
 import|;
 end_import
@@ -143,15 +171,17 @@ literal|"Fuzzy keyword search, which compares strings based on the Levenshtein d
 operator|+
 literal|"(or edit distance). The function tries to match any of the keywords specified in the "
 operator|+
-literal|"keyword string $b against the string value of each item in the sequence $a."
+literal|"keyword string against the string value of each item in the sequence $source."
 argument_list|,
 operator|new
 name|SequenceType
 index|[]
 block|{
 operator|new
-name|SequenceType
+name|FunctionParameterSequenceType
 argument_list|(
+literal|"source"
+argument_list|,
 name|Type
 operator|.
 name|NODE
@@ -159,11 +189,15 @@ argument_list|,
 name|Cardinality
 operator|.
 name|ZERO_OR_MORE
+argument_list|,
+literal|""
 argument_list|)
 block|,
 operator|new
-name|SequenceType
+name|FunctionParameterSequenceType
 argument_list|(
+literal|"keyword"
+argument_list|,
 name|Type
 operator|.
 name|STRING
@@ -171,11 +205,13 @@ argument_list|,
 name|Cardinality
 operator|.
 name|EXACTLY_ONE
+argument_list|,
+literal|"the keyword string"
 argument_list|)
 block|}
 argument_list|,
 operator|new
-name|SequenceType
+name|FunctionReturnSequenceType
 argument_list|(
 name|Type
 operator|.
@@ -184,6 +220,8 @@ argument_list|,
 name|Cardinality
 operator|.
 name|ZERO_OR_MORE
+argument_list|,
+literal|"the sequence of nodes that match the keywords"
 argument_list|)
 argument_list|,
 literal|true
