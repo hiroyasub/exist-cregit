01711f2c714f15d004792a7d2cb7abc8b3a78ea0
Dmitriy Shabanov <shabanovd@gmail.com>
2009-11-12 07:23:09 +0000

    [ignore] method to get text value
    svn path=/trunk/eXist/; revision=10457
    
    
    Former-commit-id: 432390b3025470358095fe60fd290969702e1df4

--- src/org/exist/xquery/TextConstructor.java
+++ src/org/exist/xquery/TextConstructor.java
@@ -400,6 +400,15 @@ literal|0xA
 operator|)
 return|;
 block|}
+specifier|public
+name|String
+name|getText
+parameter_list|()
+block|{
+return|return
+name|text
+return|;
+block|}
 block|}
 end_class
 
