88eb98462bcc9658473632369d5b2b5ccf6977a8
Dmitriy Shabanov <shabanovd@gmail.com>
2010-01-19 04:34:59 +0000

    [ignore] clean-up (imports & docs)
    svn path=/trunk/eXist/; revision=11029
    
    
    Former-commit-id: ffd2dd39571252ef96386fcd9c5a87d474ab7d9f

--- extensions/svn/src/org/exist/versioning/svn/ExportEditor.java
+++ extensions/svn/src/org/exist/versioning/svn/ExportEditor.java
@@ -1,6 +1,6 @@
 begin_unit|revision:1.0.0;language:Java;cregit-version:0.0.1
 begin_comment
-comment|/*  *  eXist Open Source Native XML Database  *  Copyright (C) 2009 The eXist Project  *  http://exist-db.org  *    *  This program is free software; you can redistribute it and/or  *  modify it under the terms of the GNU Lesser General Public License  *  as published by the Free Software Foundation; either version 2  *  of the License, or (at your option) any later version.  *    *  This program is distributed in the hope that it will be useful,  *  but WITHOUT ANY WARRANTY; without even the implied warranty of  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the  *  GNU Lesser General Public License for more details.  *    *  You should have received a copy of the GNU Lesser General Public License  *  along with this program; if not, write to the Free Software  *  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.  *    *  $Id:$  */
+comment|/*  *  eXist Open Source Native XML Database  *  Copyright (C) 2009-2010 The eXist Project  *  http://exist-db.org  *    *  This program is free software; you can redistribute it and/or  *  modify it under the terms of the GNU Lesser General Public License  *  as published by the Free Software Foundation; either version 2  *  of the License, or (at your option) any later version.  *    *  This program is distributed in the hope that it will be useful,  *  but WITHOUT ANY WARRANTY; without even the implied warranty of  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the  *  GNU Lesser General Public License for more details.  *    *  You should have received a copy of the GNU Lesser General Public License  *  along with this program; if not, write to the Free Software  *  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.  *    *  $Id$  */
 end_comment
 
 begin_package
@@ -14,18 +14,6 @@ operator|.
 name|svn
 package|;
 end_package
-
-begin_import
-import|import static
-name|org
-operator|.
-name|junit
-operator|.
-name|Assert
-operator|.
-name|assertNotNull
-import|;
-end_import
 
 begin_import
 import|import
@@ -827,11 +815,6 @@ operator|.
 name|getInstance
 argument_list|()
 expr_stmt|;
-name|assertNotNull
-argument_list|(
-name|pool
-argument_list|)
-expr_stmt|;
 name|broker
 operator|=
 name|pool
@@ -843,22 +826,12 @@ operator|.
 name|SYSTEM_USER
 argument_list|)
 expr_stmt|;
-name|assertNotNull
-argument_list|(
-name|broker
-argument_list|)
-expr_stmt|;
 name|transact
 operator|=
 name|pool
 operator|.
 name|getTransactionManager
 argument_list|()
-expr_stmt|;
-name|assertNotNull
-argument_list|(
-name|transact
-argument_list|)
 expr_stmt|;
 name|myRootDirectory
 operator|=
