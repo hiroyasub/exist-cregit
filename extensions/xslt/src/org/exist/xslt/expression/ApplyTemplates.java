d0720cd318158e8b22b2927f2674a0c68424e6f5
Dmitriy Shabanov <shabanovd@gmail.com>
2010-02-14 16:21:28 +0000

    [ignore] fix xslt after 'store sequence & position in it to context'
    svn path=/trunk/eXist/; revision=11226
    
    
    Former-commit-id: 8579f005231cbe9c85b8155dd4e80a06659d7c57

--- extensions/xslt/src/org/exist/xslt/expression/ApplyTemplates.java
+++ extensions/xslt/src/org/exist/xslt/expression/ApplyTemplates.java
@@ -604,9 +604,11 @@ argument_list|()
 decl_stmt|;
 name|context
 operator|.
-name|setContextPosition
+name|setContextSequencePosition
 argument_list|(
 name|pos
+argument_list|,
+name|selected
 argument_list|)
 expr_stmt|;
 name|Sequence
