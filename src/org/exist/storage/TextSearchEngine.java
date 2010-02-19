1005a2e4a7e105f24901eb4b14279f4e9cf6ab0f
Dannes Wessels <dannes@exist-db.org>
2010-02-19 22:54:44 +0000

    [ignore] small <generic> change
    svn path=/trunk/eXist/; revision=11271
    
    
    Former-commit-id: ad42ee05ee3b3fbd32ff6a9c0f0525837693a6bb

--- src/org/exist/storage/TextSearchEngine.java
+++ src/org/exist/storage/TextSearchEngine.java
@@ -278,10 +278,16 @@ argument_list|)
 decl_stmt|;
 specifier|protected
 name|TreeSet
+argument_list|<
+name|String
+argument_list|>
 name|stoplist
 init|=
 operator|new
 name|TreeSet
+argument_list|<
+name|String
+argument_list|>
 argument_list|()
 decl_stmt|;
 specifier|protected
