4a9d5a64411924a95f2ae06e65e6fc10688f3533
Adam Retter <adam.retter@googlemail.com>
2010-03-19 11:31:30 +0000

    [feature] Support for Prepared Statements in the SQL Module.
    svn path=/trunk/eXist/; revision=11478
    
    
    Former-commit-id: 7d1834c6800c6e5c66bacfe8682104d68f0b5bac

--- extensions/modules/src/org/exist/xquery/modules/sql/GetConnectionFunction.java
+++ extensions/modules/src/org/exist/xquery/modules/sql/GetConnectionFunction.java
@@ -1,6 +1,6 @@
 begin_unit|revision:1.0.0;language:Java;cregit-version:0.0.1
 begin_comment
-comment|/*  *  eXist SQL Module Extension GetConnectionFunction  *  Copyright (C) 2008-09 Adam Retter<adam@exist-db.org>  *  www.adamretter.co.uk  *    *  This program is free software; you can redistribute it and/or  *  modify it under the terms of the GNU Lesser General Public License  *  as published by the Free Software Foundation; either version 2  *  of the License, or (at your option) any later version.  *    *  This program is distributed in the hope that it will be useful,  *  but WITHOUT ANY WARRANTY; without even the implied warranty of  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the  *  GNU Lesser General Public License for more details.  *    *  You should have received a copy of the GNU Lesser General Public License  *  along with this program; if not, write to the Free Software  *  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.  *    *  $Id$  */
+comment|/*  *  eXist SQL Module Extension GetConnectionFunction  *  Copyright (C) 2008-10 Adam Retter<adam@exist-db.org>  *  www.adamretter.co.uk  *    *  This program is free software; you can redistribute it and/or  *  modify it under the terms of the GNU Lesser General Public License  *  as published by the Free Software Foundation; either version 2  *  of the License, or (at your option) any later version.  *    *  This program is distributed in the hope that it will be useful,  *  but WITHOUT ANY WARRANTY; without even the implied warranty of  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the  *  GNU Lesser General Public License for more details.  *    *  You should have received a copy of the GNU Lesser General Public License  *  along with this program; if not, write to the Free Software  *  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.  *    *  $Id$  */
 end_comment
 
 begin_package
@@ -730,7 +730,7 @@ name|dbPassword
 argument_list|)
 expr_stmt|;
 block|}
-comment|// store the connection and return the uid handle of the connection
+comment|// store the Connection and return the uid handle of the Connection
 return|return
 operator|new
 name|IntegerValue
