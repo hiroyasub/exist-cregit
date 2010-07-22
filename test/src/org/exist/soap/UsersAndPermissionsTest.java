52f7ccce3ab772afab849c2ad02155c6c174abfe
Dmitriy Shabanov <shabanovd@gmail.com>
2010-07-22 16:26:11 +0000

    [ignore] workaround soap's missing roles manipulation ways
    svn path=/trunk/eXist/; revision=12023
    
    
    Former-commit-id: 2b7879b957e9d2219e4d810ed2ab3ba5e678fc4f

--- test/src/org/exist/soap/UsersAndPermissionsTest.java
+++ test/src/org/exist/soap/UsersAndPermissionsTest.java
@@ -296,15 +296,6 @@ name|rex
 parameter_list|)
 block|{
 block|}
-name|admin
-operator|.
-name|getUser
-argument_list|(
-name|sessionId
-argument_list|,
-name|testUser
-argument_list|)
-expr_stmt|;
 name|String
 index|[]
 name|testGroups
