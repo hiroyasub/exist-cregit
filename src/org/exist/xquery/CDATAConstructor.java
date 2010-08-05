6ba6911f2f9b947c099860b415db638d31cfd9a4
Dmitriy Shabanov <shabanovd@gmail.com>
2010-08-05 05:03:44 +0000

    [bugfix] ... the content of an element constructor is processed ... 1. ... b. ... Characters inside a CDataSection, including special characters such as < and &, are treated as literal characters rather than as markup characters (except for the sequence ]]>, which terminates the CDataSection)
    XQuery 1.0: An XML Query Language - 3.7.1.3 Content
    
    svn path=/trunk/eXist/; revision=12226
    
    
    Former-commit-id: f61ec6172d4449a129d12e2aa822cb81d20fa229

--- src/org/exist/xquery/CDATAConstructor.java
+++ src/org/exist/xquery/CDATAConstructor.java
@@ -1,6 +1,6 @@
 begin_unit|revision:1.0.0;language:Java;cregit-version:0.0.1
 begin_comment
-comment|/*  *  eXist Open Source Native XML Database  *  Copyright (C) 2001-04 The eXist Project  *  http://exist-db.org  *    *  This program is free software; you can redistribute it and/or  *  modify it under the terms of the GNU Lesser General Public License  *  as published by the Free Software Foundation; either version 2  *  of the License, or (at your option) any later version.  *    *  This program is distributed in the hope that it will be useful,  *  but WITHOUT ANY WARRANTY; without even the implied warranty of  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the  *  GNU Lesser General Public License for more details.  *    *  You should have received a copy of the GNU Lesser General Public License  *  along with this program; if not, write to the Free Software  *  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.  *    *  $Id$  */
+comment|/*  *  eXist Open Source Native XML Database  *  Copyright (C) 2001-2010 The eXist Project  *  http://exist-db.org  *    *  This program is free software; you can redistribute it and/or  *  modify it under the terms of the GNU Lesser General Public License  *  as published by the Free Software Foundation; either version 2  *  of the License, or (at your option) any later version.  *    *  This program is distributed in the hope that it will be useful,  *  but WITHOUT ANY WARRANTY; without even the implied warranty of  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the  *  GNU Lesser General Public License for more details.  *    *  You should have received a copy of the GNU Lesser General Public License  *  along with this program; if not, write to the Free Software  *  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.  *    *  $Id$  */
 end_comment
 
 begin_package
@@ -94,6 +94,12 @@ specifier|private
 specifier|final
 name|String
 name|cdata
+decl_stmt|;
+specifier|private
+name|boolean
+name|literalCharacters
+init|=
+literal|false
 decl_stmt|;
 comment|/**      * @param context      */
 specifier|public
@@ -252,14 +258,34 @@ argument_list|()
 decl_stmt|;
 name|int
 name|nodeNr
-init|=
+decl_stmt|;
+if|if
+condition|(
+name|literalCharacters
+condition|)
+block|{
+name|nodeNr
+operator|=
+name|builder
+operator|.
+name|characters
+argument_list|(
+name|cdata
+argument_list|)
+expr_stmt|;
+block|}
+else|else
+block|{
+name|nodeNr
+operator|=
 name|builder
 operator|.
 name|cdataSection
 argument_list|(
 name|cdata
 argument_list|)
-decl_stmt|;
+expr_stmt|;
+block|}
 name|NodeImpl
 name|node
 init|=
@@ -331,6 +357,19 @@ name|analyze
 argument_list|(
 name|contextInfo
 argument_list|)
+expr_stmt|;
+name|literalCharacters
+operator|=
+operator|(
+name|contextInfo
+operator|.
+name|getFlags
+argument_list|()
+operator|&
+name|IN_NODE_CONSTRUCTOR
+operator|)
+operator|!=
+literal|0
 expr_stmt|;
 block|}
 comment|/* (non-Javadoc)      * @see org.exist.xquery.Expression#dump(org.exist.xquery.util.ExpressionDumper)      */
