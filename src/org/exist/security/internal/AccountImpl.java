e9f7e3100032048530f6a786943f7c13446a6a6e
Dmitriy Shabanov <shabanovd@gmail.com>
2010-08-21 14:30:18 +0000

    [ignore] force to use AbstractAccount's equals on SubjectImpl
    svn path=/trunk/eXist/; revision=12504
    
    
    Former-commit-id: 9050f67d895b18f1577f9001c81a1dae45841a44

--- src/org/exist/security/internal/AccountImpl.java
+++ src/org/exist/security/internal/AccountImpl.java
@@ -289,16 +289,6 @@ name|java
 operator|.
 name|util
 operator|.
-name|HashSet
-import|;
-end_import
-
-begin_import
-import|import
-name|java
-operator|.
-name|util
-operator|.
 name|Properties
 import|;
 end_import
