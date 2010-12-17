5b89050fcc4df243e8915166b878b77ffffad022
Wolfgang Meier <wolfgangmm@gmail.com>
2010-12-17 19:50:40 +0000

    [bugfix] Fixed a few cases in which a temporary fragment was generated.
    svn path=/stable/eXist-1.4.x/; revision=13410
    
    
    Former-commit-id: 5c871f2931a2adefc3d7c34c1b67503a51b37158

--- extensions/indexes/lucene/src/org/exist/xquery/modules/lucene/Query.java
+++ extensions/indexes/lucene/src/org/exist/xquery/modules/lucene/Query.java
@@ -1044,6 +1044,20 @@ parameter_list|)
 throws|throws
 name|XPathException
 block|{
+if|if
+condition|(
+operator|!
+name|contextSequence
+operator|.
+name|isPersistentSet
+argument_list|()
+condition|)
+comment|// in-memory docs won't have an index
+return|return
+name|NodeSet
+operator|.
+name|EMPTY_SET
+return|;
 name|long
 name|start
 init|=
@@ -1314,6 +1328,24 @@ operator|.
 name|toSequence
 argument_list|()
 expr_stmt|;
+if|if
+condition|(
+name|contextSequence
+operator|!=
+literal|null
+operator|&&
+operator|!
+name|contextSequence
+operator|.
+name|isPersistentSet
+argument_list|()
+condition|)
+comment|// in-memory docs won't have an index
+return|return
+name|Sequence
+operator|.
+name|EMPTY_SEQUENCE
+return|;
 name|NodeSet
 name|result
 decl_stmt|;
