6ce6d46f0b4346dcef95c13054be4423e65556ea
Dannes Wessels <dannes@exist-db.org>
2010-12-29 21:41:05 +0000

    [bugfix] Prevent ": The InputStream has been closed" exception in get-data()
    Caused by: java.io.IOException: The InputStream has been closed
            at org.exist.util.io.CachingFilterInputStream.read(CachingFilterInputStream.java:100)
            at org.exist.util.io.CachingFilterInputStream.read(CachingFilterInputStream.java:93)
            at org.exist.xquery.functions.request.GetData.eval(GetData.java:213)
            ... 56 more
    
    svn path=/trunk/eXist/; revision=13470
    
    
    Former-commit-id: e7830ad2555ca7e3380066f65227a156beddfc2d

--- src/org/exist/xquery/functions/request/GetData.java
+++ src/org/exist/xquery/functions/request/GetData.java
@@ -764,6 +764,13 @@ condition|(
 name|is
 operator|!=
 literal|null
+operator|&&
+name|is
+operator|.
+name|available
+argument_list|()
+operator|>
+literal|0
 condition|)
 block|{
 comment|//determine if exists mime database considers this binary data
