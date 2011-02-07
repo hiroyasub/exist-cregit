39a2736e2918655006154ebb86c360e2d6e591fe
Dmitriy Shabanov <shabanovd@gmail.com>
2011-02-07 07:10:51 +0000

    [ignore] fix NPE on unexist db resource
    svn path=/trunk/eXist/; revision=13734
    
    
    Former-commit-id: 60e7e810c3bbabee3e1086f18188efd444e68e76

--- src/org/exist/xquery/functions/transform/Transform.java
+++ src/org/exist/xquery/functions/transform/Transform.java
@@ -1,6 +1,6 @@
 begin_unit|revision:1.0.0;language:Java;cregit-version:0.0.1
 begin_comment
-comment|/*  *  eXist Open Source Native XML Database  *  Copyright (C) 2001-09 Wolfgang M. Meier  *  wolfgang@exist-db.org  *  http://exist.sourceforge.net  *    *  This program is free software; you can redistribute it and/or  *  modify it under the terms of the GNU Lesser General Public License  *  as published by the Free Software Foundation; either version 2  *  of the License, or (at your option) any later version.  *    *  This program is distributed in the hope that it will be useful,  *  but WITHOUT ANY WARRANTY; without even the implied warranty of  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the  *  GNU Lesser General Public License for more details.  *    *  You should have received a copy of the GNU Lesser General Public License  *  along with this program; if not, write to the Free Software  *  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.  *    *  $Id$  */
+comment|/*  *  eXist Open Source Native XML Database  *  Copyright (C) 2001-2011 The eXist Project  *  http://exist-db.org  *  *  This program is free software; you can redistribute it and/or  *  modify it under the terms of the GNU Lesser General Public License  *  as published by the Free Software Foundation; either version 2  *  of the License, or (at your option) any later version.  *  *  This program is distributed in the hope that it will be useful,  *  but WITHOUT ANY WARRANTY; without even the implied warranty of  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the  *  GNU Lesser General Public License for more details.  *  *  You should have received a copy of the GNU Lesser General Public  *  License along with this library; if not, write to the Free Software  *  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA  *  *  $Id$  */
 end_comment
 
 begin_package
@@ -362,18 +362,6 @@ operator|.
 name|log4j
 operator|.
 name|Logger
-import|;
-end_import
-
-begin_import
-import|import
-name|org
-operator|.
-name|exist
-operator|.
-name|collections
-operator|.
-name|Collection
 import|;
 end_import
 
@@ -4624,27 +4612,17 @@ operator|+
 literal|" not found"
 argument_list|)
 expr_stmt|;
-name|Collection
-name|collection
-init|=
-name|context
-operator|.
-name|getBroker
-argument_list|()
-operator|.
-name|getCollection
+throw|throw
+operator|new
+name|TransformerException
 argument_list|(
-name|uri
+literal|"Resource "
+operator|+
+name|path
+operator|+
+literal|" not found in database."
 argument_list|)
-decl_stmt|;
-if|if
-condition|(
-name|collection
-operator|!=
-literal|null
-condition|)
-block|{
-block|}
+throw|;
 block|}
 if|if
 condition|(
