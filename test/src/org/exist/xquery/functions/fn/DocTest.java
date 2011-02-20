2f391402f81c12204ca2fac980189775507005a5
Dmitriy Shabanov <shabanovd@gmail.com>
2011-02-20 15:11:52 +0000

    [ignore] disable remote 'doc' function test, because of conflict with jetty
    svn path=/trunk/eXist/; revision=13792
    
    
    Former-commit-id: cecc01a29aa8aab719352eacef4d96a4700d6a5b

--- test/src/org/exist/xquery/functions/fn/DocTest.java
+++ test/src/org/exist/xquery/functions/fn/DocTest.java
@@ -53,6 +53,16 @@ name|org
 operator|.
 name|junit
 operator|.
+name|Ignore
+import|;
+end_import
+
+begin_import
+import|import
+name|org
+operator|.
+name|junit
+operator|.
 name|Test
 import|;
 end_import
@@ -790,6 +800,8 @@ argument_list|)
 expr_stmt|;
 block|}
 block|}
+annotation|@
+name|Ignore
 annotation|@
 name|Test
 specifier|public
