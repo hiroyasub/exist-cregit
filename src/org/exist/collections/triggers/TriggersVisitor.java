1882bfc944408fb9ef7b020975c34797f1bf34d3
Adam Retter <adam.retter@googlemail.com>
2012-01-18 03:02:00 +0000

    [feature] Support to configure multiple Document and Collection triggers for a Collection
    svn path=/trunk/eXist/; revision=15687
    
    
    Former-commit-id: 719f67ae4051a0f768d75a2f02a8d619b59b1548

---
+++ src/org/exist/collections/triggers/TriggersVisitor.java
@@ -0,0 +1,28 @@
+begin_unit|revision:1.0.0;language:Java;cregit-version:0.0.1
+begin_package
+package|package
+name|org
+operator|.
+name|exist
+operator|.
+name|collections
+operator|.
+name|triggers
+package|;
+end_package
+
+begin_comment
+comment|/**  *  * @author aretter  */
+end_comment
+
+begin_interface
+specifier|public
+interface|interface
+name|TriggersVisitor
+extends|extends
+name|Trigger
+block|{      }
+end_interface
+
+end_unit
+
