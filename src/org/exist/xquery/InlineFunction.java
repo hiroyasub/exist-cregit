94b5db5138db35df0fe15d812599329b1d5001b0
Wolfgang Meier <wolfgangmm@gmail.com>
2012-03-27 10:08:40 +0000

    [feature] XQuery 3.0 inline functions: support closures (local context variables are copied into the context of the inline function).
    svn path=/trunk/eXist/; revision=16196
    
    
    Former-commit-id: 6ed13e323127d9b145c9fe14037edd9827def81a

--- src/org/exist/xquery/InlineFunction.java
+++ src/org/exist/xquery/InlineFunction.java
@@ -1,4 +1,8 @@
 begin_unit|revision:1.0.0;language:Java;cregit-version:0.0.1
+begin_comment
+comment|/*  *  eXist Open Source Native XML Database  *  Copyright (C) 2012 The eXist Project  *  http://exist-db.org  *  *  This program is free software; you can redistribute it and/or  *  modify it under the terms of the GNU Lesser General Public License  *  as published by the Free Software Foundation; either version 2  *  of the License, or (at your option) any later version.  *  *  This program is distributed in the hope that it will be useful,  *  but WITHOUT ANY WARRANTY; without even the implied warranty of  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the  *  GNU Lesser General Public License for more details.  *  *  You should have received a copy of the GNU Lesser General Public  *  License along with this library; if not, write to the Free Software  *  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA  *  *  $Id$  */
+end_comment
+
 begin_package
 package|package
 name|org
@@ -8,6 +12,16 @@ operator|.
 name|xquery
 package|;
 end_package
+
+begin_import
+import|import
+name|java
+operator|.
+name|util
+operator|.
+name|List
+import|;
+end_import
 
 begin_import
 import|import
@@ -79,6 +93,10 @@ name|Type
 import|;
 end_import
 
+begin_comment
+comment|/**  * An XQuery 3.0 inline function expression.  *   * @author wolf  *  */
+end_comment
+
 begin_class
 specifier|public
 class|class
@@ -140,6 +158,25 @@ argument_list|(
 name|SINGLE_STEP_EXECUTION
 argument_list|)
 expr_stmt|;
+comment|// local variable context is known within inline function:
+name|List
+argument_list|<
+name|Variable
+argument_list|>
+name|closureVars
+init|=
+name|context
+operator|.
+name|getLocalStack
+argument_list|()
+decl_stmt|;
+name|function
+operator|.
+name|setClosureVariables
+argument_list|(
+name|closureVars
+argument_list|)
+expr_stmt|;
 name|function
 operator|.
 name|analyze
@@ -173,8 +210,7 @@ name|dumper
 argument_list|)
 expr_stmt|;
 block|}
-annotation|@
-name|Override
+comment|/** 	 * Wraps a function call around the function and returns a 	 * reference to it. Make sure local variables in the context 	 * are visible. 	 */
 specifier|public
 name|Sequence
 name|eval
@@ -188,6 +224,25 @@ parameter_list|)
 throws|throws
 name|XPathException
 block|{
+comment|// local variable context is known within inline function
+name|List
+argument_list|<
+name|Variable
+argument_list|>
+name|closureVars
+init|=
+name|context
+operator|.
+name|getLocalStack
+argument_list|()
+decl_stmt|;
+name|function
+operator|.
+name|setClosureVariables
+argument_list|(
+name|closureVars
+argument_list|)
+expr_stmt|;
 name|FunctionCall
 name|call
 init|=
