d7ff0c3df87e1b4c08e55e6ed3f9ef7ff543c320
Dmitriy Shabanov <shabanovd@gmail.com>
2012-05-20 17:35:08 +0000

    [bugfix] expressions under enclosed expression must create new builder
    svn path=/trunk/eXist/; revision=16424
    
    
    Former-commit-id: f93daed6e73c0d2d1b20f6d2ae19fccb459ae4cd

--- src/org/exist/xquery/EnclosedExpr.java
+++ src/org/exist/xquery/EnclosedExpr.java
@@ -1,6 +1,6 @@
 begin_unit|revision:1.0.0;language:Java;cregit-version:0.0.1
 begin_comment
-comment|/*  *  eXist Native XML Database  *  Copyright (C) 2001-06,  Wolfgang M. Meier (wolfgang@exist-db.org)  *  *  This library is free software; you can redistribute it and/or  *  modify it under the terms of the GNU Library General Public License  *  as published by the Free Software Foundation; either version 2  *  of the License, or (at your option) any later version.  *  *  This library is distributed in the hope that it will be useful,  *  but WITHOUT ANY WARRANTY; without even the implied warranty of  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the  *  GNU Library General Public License for more details.  *  *  You should have received a copy of the GNU General Public License  *  along with this program; if not, write to the Free Software  *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.  *   * $Id$  */
+comment|/*  *  eXist Open Source Native XML Database  *  Copyright (C) 2001-2012 The eXist Project  *  http://exist-db.org  *  *  This program is free software; you can redistribute it and/or  *  modify it under the terms of the GNU Lesser General Public License  *  as published by the Free Software Foundation; either version 2  *  of the License, or (at your option) any later version.  *  *  This program is distributed in the hope that it will be useful,  *  but WITHOUT ANY WARRANTY; without even the implied warranty of  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the  *  GNU Lesser General Public License for more details.  *  *  You should have received a copy of the GNU Lesser General Public  *  License along with this library; if not, write to the Free Software  *  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA  *  *  $Id$  */
 end_comment
 
 begin_package
@@ -141,6 +141,40 @@ block|{
 name|super
 argument_list|(
 name|context
+argument_list|)
+expr_stmt|;
+block|}
+specifier|public
+name|void
+name|analyze
+parameter_list|(
+name|AnalyzeContextInfo
+name|contextInfo
+parameter_list|)
+throws|throws
+name|XPathException
+block|{
+name|AnalyzeContextInfo
+name|newContextInfo
+init|=
+operator|new
+name|AnalyzeContextInfo
+argument_list|(
+name|contextInfo
+argument_list|)
+decl_stmt|;
+name|newContextInfo
+operator|.
+name|removeFlag
+argument_list|(
+name|IN_NODE_CONSTRUCTOR
+argument_list|)
+expr_stmt|;
+name|super
+operator|.
+name|analyze
+argument_list|(
+name|newContextInfo
 argument_list|)
 expr_stmt|;
 block|}
