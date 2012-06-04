1384509eb224681be60625bb2beefc17d3fb0bed
Dmitriy Shabanov <shabanovd@gmail.com>
2012-06-04 19:35:12 +0000

    [ignore] avoid NPE
    svn path=/trunk/eXist/; revision=16598
    
    
    Former-commit-id: b0ab7a63ac86e0ccb8bef0882a07dc3e57dc164c

--- src/org/exist/xquery/DocumentConstructor.java
+++ src/org/exist/xquery/DocumentConstructor.java
@@ -375,25 +375,6 @@ try|try
 block|{
 if|if
 condition|(
-name|contentSeq
-operator|.
-name|isEmpty
-argument_list|()
-condition|)
-block|{
-name|receiver
-operator|.
-name|startDocument
-argument_list|()
-expr_stmt|;
-name|receiver
-operator|.
-name|endDocument
-argument_list|()
-expr_stmt|;
-block|}
-if|else if
-condition|(
 operator|!
 name|contentSeq
 operator|.
