b9c3f7c5e5292cbb594ba34b17083a40eaf72792
Dmitriy Shabanov <shabanovd@gmail.com>
2012-06-13 17:04:56 +0000

    [ignore] cleanup
    svn path=/trunk/eXist/; revision=16666
    
    
    Former-commit-id: a380cc880fd8f26b498afb1f21488cc3573f54f6

--- src/org/exist/dom/MutableDocumentSet.java
+++ src/org/exist/dom/MutableDocumentSet.java
@@ -1,4 +1,8 @@
 begin_unit|revision:1.0.0;language:Java;cregit-version:0.0.1
+begin_comment
+comment|/*  *  eXist Open Source Native XML Database  *  Copyright (C) 2000-2012 The eXist Project  *  http://exist-db.org  *  *  This program is free software; you can redistribute it and/or  *  modify it under the terms of the GNU Lesser General Public License  *  as published by the Free Software Foundation; either version 2  *  of the License, or (at your option) any later version.  *  *  This program is distributed in the hope that it will be useful,  *  but WITHOUT ANY WARRANTY; without even the implied warranty of  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the  *  GNU Lesser General Public License for more details.  *  *  You should have received a copy of the GNU Lesser General Public  *  License along with this library; if not, write to the Free Software  *  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA  *  *  $Id$  */
+end_comment
+
 begin_package
 package|package
 name|org
@@ -21,46 +25,8 @@ name|Collection
 import|;
 end_import
 
-begin_import
-import|import
-name|org
-operator|.
-name|exist
-operator|.
-name|storage
-operator|.
-name|DBBroker
-import|;
-end_import
-
-begin_import
-import|import
-name|org
-operator|.
-name|exist
-operator|.
-name|storage
-operator|.
-name|lock
-operator|.
-name|LockedDocumentMap
-import|;
-end_import
-
-begin_import
-import|import
-name|org
-operator|.
-name|exist
-operator|.
-name|util
-operator|.
-name|LockException
-import|;
-end_import
-
 begin_comment
-comment|/**  *   */
+comment|/**  * Manages a set of documents.  *   * @author wolf  */
 end_comment
 
 begin_interface
