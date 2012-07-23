f6acc7917d60f9351a99530b5c0952cf3ad51492
Dmitriy Shabanov <shabanovd@gmail.com>
2012-07-23 10:35:46 +0000

    [ignore] cleanup
    svn path=/trunk/eXist/; revision=16870
    
    
    Former-commit-id: e26bfb59474a4c99f4bccb1fce8bf71b9782870c

--- src/org/exist/storage/SystemTaskManager.java
+++ src/org/exist/storage/SystemTaskManager.java
@@ -1,4 +1,8 @@
 begin_unit|revision:1.0.0;language:Java;cregit-version:0.0.1
+begin_comment
+comment|/*  *  eXist Open Source Native XML Database  *  Copyright (C) 2012 The eXist Project  *  http://exist-db.org  *  *  This program is free software; you can redistribute it and/or  *  modify it under the terms of the GNU Lesser General Public License  *  as published by the Free Software Foundation; either version 2  *  of the License, or (at your option) any later version.  *  *  This program is distributed in the hope that it will be useful,  *  but WITHOUT ANY WARRANTY; without even the implied warranty of  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the  *  GNU Lesser General Public License for more details.  *  *  You should have received a copy of the GNU Lesser General Public  *  License along with this library; if not, write to the Free Software  *  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA  *  *  $Id$  */
+end_comment
+
 begin_package
 package|package
 name|org
@@ -42,20 +46,6 @@ operator|.
 name|sync
 operator|.
 name|Sync
-import|;
-end_import
-
-begin_import
-import|import
-name|org
-operator|.
-name|exist
-operator|.
-name|storage
-operator|.
-name|sync
-operator|.
-name|SyncTask
 import|;
 end_import
 
@@ -190,7 +180,7 @@ argument_list|()
 expr_stmt|;
 name|broker
 operator|.
-name|setUser
+name|setSubject
 argument_list|(
 name|pool
 operator|.
@@ -279,7 +269,7 @@ condition|)
 block|{
 name|broker
 operator|.
-name|setUser
+name|setSubject
 argument_list|(
 name|oldUser
 argument_list|)
