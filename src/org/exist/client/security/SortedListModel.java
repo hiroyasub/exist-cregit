a92d1004a52e44877290c128dd78171e009302e3
Dmitriy Shabanov <shabanovd@gmail.com>
2012-11-15 09:45:50 +0000

    [ignore] cleanup
    svn path=/trunk/eXist/; revision=17596
    
    
    Former-commit-id: f3d158b2a11ff3df5a7974e5bc973820dcaecc9c

--- src/org/exist/client/security/SortedListModel.java
+++ src/org/exist/client/security/SortedListModel.java
@@ -1,4 +1,8 @@
 begin_unit|revision:1.0.0;language:Java;cregit-version:0.0.1
+begin_comment
+comment|/*  *  eXist Open Source Native XML Database  *  Copyright (C) 2001-2012 The eXist Project  *  http://exist-db.org  *  *  This program is free software; you can redistribute it and/or  *  modify it under the terms of the GNU Lesser General Public License  *  as published by the Free Software Foundation; either version 2  *  of the License, or (at your option) any later version.  *  *  This program is distributed in the hope that it will be useful,  *  but WITHOUT ANY WARRANTY; without even the implied warranty of  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the  *  GNU Lesser General Public License for more details.  *  *  You should have received a copy of the GNU Lesser General Public  *  License along with this library; if not, write to the Free Software  *  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA  *  *  $Id$  */
+end_comment
+
 begin_package
 package|package
 name|org
@@ -83,6 +87,14 @@ parameter_list|>
 extends|extends
 name|AbstractListModel
 block|{
+specifier|private
+specifier|static
+specifier|final
+name|long
+name|serialVersionUID
+init|=
+literal|8156990970750901747L
+decl_stmt|;
 specifier|private
 specifier|final
 name|SortedSet
