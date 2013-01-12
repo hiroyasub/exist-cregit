8246a29121d0ff46bf785f7af0df1d62779bbe3a
Dmitriy Shabanov <shabanovd@gmail.com>
2013-01-12 16:05:57 +0000

    [ignore] add system:import-silently xquery function
    svn path=/trunk/eXist/; revision=18052
    
    
    Former-commit-id: 592237b3074d2ce07b46832eda3b6707b9b11a2a

--- src/org/exist/xquery/functions/system/FnExport.java
+++ src/org/exist/xquery/functions/system/FnExport.java
@@ -1,6 +1,6 @@
 begin_unit|revision:1.0.0;language:Java;cregit-version:0.0.1
 begin_comment
-comment|/*  *  eXist Open Source Native XML Database  *  Copyright (C) 2012 The eXist Project  *  http://exist-db.org  *  *  This program is free software; you can redistribute it and/or  *  modify it under the terms of the GNU Lesser General Public License  *  as published by the Free Software Foundation; either version 2  *  of the License, or (at your option) any later version.  *  *  This program is distributed in the hope that it will be useful,  *  but WITHOUT ANY WARRANTY; without even the implied warranty of  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the  *  GNU Lesser General Public License for more details.  *  *  You should have received a copy of the GNU Lesser General Public  *  License along with this library; if not, write to the Free Software  *  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA  *  *  $Id$  */
+comment|/*  *  eXist Open Source Native XML Database  *  Copyright (C) 2012-2013 The eXist Project  *  http://exist-db.org  *  *  This program is free software; you can redistribute it and/or  *  modify it under the terms of the GNU Lesser General Public License  *  as published by the Free Software Foundation; either version 2  *  of the License, or (at your option) any later version.  *  *  This program is distributed in the hope that it will be useful,  *  but WITHOUT ANY WARRANTY; without even the implied warranty of  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the  *  GNU Lesser General Public License for more details.  *  *  You should have received a copy of the GNU Lesser General Public  *  License along with this library; if not, write to the Free Software  *  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA  *  *  $Id$  */
 end_comment
 
 begin_package
@@ -417,7 +417,7 @@ argument_list|)
 argument_list|,
 name|DESCRIPTION
 operator|+
-literal|"Messagers from exporter reroute to logs."
+literal|" Messagers from exporter reroute to logs."
 argument_list|,
 operator|new
 name|SequenceType
@@ -841,6 +841,7 @@ name|builder
 operator|==
 literal|null
 condition|)
+block|{
 name|SystemExport
 operator|.
 name|LOG
@@ -852,6 +853,7 @@ operator|+
 name|path
 argument_list|)
 expr_stmt|;
+block|}
 else|else
 block|{
 name|builder
@@ -901,6 +903,7 @@ name|builder
 operator|==
 literal|null
 condition|)
+block|{
 name|SystemExport
 operator|.
 name|LOG
@@ -912,6 +915,7 @@ operator|+
 name|name
 argument_list|)
 expr_stmt|;
+block|}
 else|else
 block|{
 name|builder
@@ -956,6 +960,7 @@ name|builder
 operator|==
 literal|null
 condition|)
+block|{
 name|SystemExport
 operator|.
 name|LOG
@@ -967,6 +972,7 @@ argument_list|,
 name|exception
 argument_list|)
 expr_stmt|;
+block|}
 else|else
 block|{
 name|builder
