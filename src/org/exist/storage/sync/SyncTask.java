69c7fe136ff57e62de13c4e20ec4062ff6e37b29
Leif-Jöran Olsson <ljo@fripost.org>
2013-02-05 23:20:56 +0000

    [bugfix] Check free space on partition containing DATA_DIR instead of EXIST_HOME ancestor root partition. Should probably also check the one containing JOURNAL_DIR.
    svn path=/trunk/eXist/; revision=18233
    
    
    Former-commit-id: f2bec15cb1fbf36ac902dbfaf9b3b42400a7cbd6

--- src/org/exist/storage/sync/SyncTask.java
+++ src/org/exist/storage/sync/SyncTask.java
@@ -1,4 +1,8 @@
 begin_unit|revision:1.0.0;language:Java;cregit-version:0.0.1
+begin_comment
+comment|/*  * eXist Open Source Native XML Database  * Copyright (C) 2011-2013 The eXist-db Project  * http://exist-db.org  *  * This program is free software; you can redistribute it and/or  * modify it under the terms of the GNU Lesser General Public License  * as published by the Free Software Foundation; either version 2  * of the License, or (at your option) any later version.  *    * This program is distributed in the hope that it will be useful,  * but WITHOUT ANY WARRANTY; without even the implied warranty of  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the  * GNU Lesser General Public License for more details.  *   * You should have received a copy of the GNU Lesser General Public License  * along with this program; if not, write to the Free Software Foundation  * Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.  *    *  $Id$  */
+end_comment
+
 begin_package
 package|package
 name|org
@@ -134,7 +138,7 @@ return|;
 block|}
 specifier|private
 name|File
-name|partition
+name|dataDir
 decl_stmt|;
 specifier|private
 name|long
@@ -202,52 +206,33 @@ literal|1024L
 operator|*
 literal|1024L
 expr_stmt|;
+comment|// fixme! - Shouldn't it be data dir AND journal dir we check
+comment|// rather than EXIST_HOME? /ljo
+name|dataDir
+operator|=
+operator|new
 name|File
-name|homeDir
-init|=
+argument_list|(
+operator|(
+name|String
+operator|)
 name|config
 operator|.
-name|getExistHome
-argument_list|()
-decl_stmt|;
-name|partition
-operator|=
-name|homeDir
-expr_stmt|;
-name|File
-name|parent
-init|=
-name|homeDir
+name|getProperty
+argument_list|(
+name|BrokerPool
 operator|.
-name|getParentFile
-argument_list|()
-decl_stmt|;
-while|while
-condition|(
-name|parent
-operator|!=
-literal|null
-condition|)
-block|{
-name|partition
-operator|=
-name|parent
+name|PROPERTY_DATA_DIR
+argument_list|)
+argument_list|)
 expr_stmt|;
-name|parent
-operator|=
-name|parent
-operator|.
-name|getParentFile
-argument_list|()
-expr_stmt|;
-block|}
 name|LOG
 operator|.
 name|info
 argument_list|(
-literal|"Using Partition: "
+literal|"Using DATA_DIR: "
 operator|+
-name|partition
+name|dataDir
 operator|.
 name|getAbsolutePath
 argument_list|()
@@ -256,7 +241,47 @@ literal|". Minimal disk space required for database "
 operator|+
 literal|"to continue operations: "
 operator|+
+operator|(
 name|diskSpaceMin
+operator|/
+literal|1024
+operator|/
+literal|1024
+operator|)
+operator|+
+literal|"mb"
+argument_list|)
+expr_stmt|;
+name|long
+name|space
+init|=
+name|dataDir
+operator|.
+name|getUsableSpace
+argument_list|()
+decl_stmt|;
+name|LOG
+operator|.
+name|info
+argument_list|(
+literal|"Usable space on partition containing DATA_DIR: "
+operator|+
+name|dataDir
+operator|.
+name|getAbsolutePath
+argument_list|()
+operator|+
+literal|": "
+operator|+
+operator|(
+name|space
+operator|/
+literal|1024
+operator|/
+literal|1024
+operator|)
+operator|+
+literal|"mb"
 argument_list|)
 expr_stmt|;
 block|}
@@ -293,9 +318,9 @@ name|LOG
 operator|.
 name|fatal
 argument_list|(
-literal|"Partition "
+literal|"Partition containing DATA_DIR: "
 operator|+
-name|partition
+name|dataDir
 operator|.
 name|getAbsolutePath
 argument_list|()
@@ -367,12 +392,12 @@ block|{
 name|long
 name|space
 init|=
-name|partition
+name|dataDir
 operator|.
 name|getUsableSpace
 argument_list|()
 decl_stmt|;
-comment|//LOG.info("Usable space on partition " + partition.getAbsolutePath() + ": " + (space / 1024 / 1024) + "mb");
+comment|//LOG.info("Usable space on partition containing DATA_DIR: " + dataDir.getAbsolutePath() + ": " + (space / 1024 / 1024) + "mb");
 return|return
 name|space
 operator|>
