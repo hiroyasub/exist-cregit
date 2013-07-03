6a1419c08b31f3db74d10c3a7b4d70f919882c60
Leif-Jöran Olsson <ljo@fripost.org>
2013-07-03 15:48:27 +0000

    [ignore] Proofing and LGP header.
    svn path=/trunk/eXist/; revision=18645
    
    
    Former-commit-id: 2688cdaf0a3b80c559ed087fbcb896b6daf27db5

--- extensions/modules/src/org/exist/xquery/modules/httpclient/ClearFunction.java
+++ extensions/modules/src/org/exist/xquery/modules/httpclient/ClearFunction.java
@@ -1,6 +1,6 @@
 begin_unit|revision:1.0.0;language:Java;cregit-version:0.0.1
 begin_comment
-comment|/*  *  eXist Open Source Native XML Database  *  Copyright (C) 2001-09 Wolfgang M. Meier  *  wolfgang@exist-db.org  *  http://exist.sourceforge.net  *  *  This program is free software; you can redistribute it and/or  *  modify it under the terms of the GNU Lesser General Public License  *  as published by the Free Software Foundation; either version 2  *  of the License, or (at your option) any later version.  *  *  This program is distributed in the hope that it will be useful,  *  but WITHOUT ANY WARRANTY; without even the implied warranty of  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the  *  GNU Lesser General Public License for more details.  *  *  You should have received a copy of the GNU Lesser General Public License  *  along with this program; if not, write to the Free Software  *  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.  *  *  $Id: ClearPersistentCookiesFunction.java 9680 2009-08-06 19:23:51Z ixitar $  */
+comment|/*  * eXist Open Source Native XML Database  * Copyright (C) 2010-2013 The eXist-db Project  * http://exist-db.org  *  * This program is free software; you can redistribute it and/or  * modify it under the terms of the GNU Lesser General Public License  * as published by the Free Software Foundation; either version 2  * of the License, or (at your option) any later version.  *    * This program is distributed in the hope that it will be useful,  * but WITHOUT ANY WARRANTY; without even the implied warranty of  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the  * GNU Lesser General Public License for more details.  *   * You should have received a copy of the GNU Lesser General Public License  * along with this program; if not, write to the Free Software Foundation  * Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.  *    *  $Id$  */
 end_comment
 
 begin_package
@@ -192,7 +192,7 @@ argument_list|,
 name|PREFIX
 argument_list|)
 argument_list|,
-literal|"Clears all persistent state (eg. cookies, crednetials, etc.) stored in the current session on the client."
+literal|"Clears all persistent state (eg. cookies, credentials, etc.) stored in the current session on the client."
 argument_list|,
 literal|null
 argument_list|,
