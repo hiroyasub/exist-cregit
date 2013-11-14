c82f2c915c973dca2eb788162581f73848d64d38
Adam Retter <adam.retter@googlemail.com>
2013-11-14 12:10:08 +0000

    [javadoc] Added licence header and some detail on intention of the class
    Former-commit-id: 7c3a0a9b7d43a8441d17820ffea3c187509c443b

--- src/org/exist/security/EffectiveSubject.java
+++ src/org/exist/security/EffectiveSubject.java
@@ -1,4 +1,8 @@
 begin_unit|revision:1.0.0;language:Java;cregit-version:0.0.1
+begin_comment
+comment|/*  *  eXist Open Source Native XML Database  *  Copyright (C) 2013 The eXist-db Project  *  http://exist-db.org  *    *  This program is free software; you can redistribute it and/or  *  modify it under the terms of the GNU Lesser General Public License  *  as published by the Free Software Foundation; either version 2  *  of the License, or (at your option) any later version.  *    *  This program is distributed in the hope that it will be useful,  *  but WITHOUT ANY WARRANTY; without even the implied warranty of  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the  *  GNU Lesser General Public License for more details.  *    *  You should have received a copy of the GNU Lesser General Public License  *  along with this program; if not, write to the Free Software  *  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.  *   *  $Id$  */
+end_comment
+
 begin_package
 package|package
 name|org
@@ -68,6 +72,10 @@ operator|.
 name|DBBroker
 import|;
 end_import
+
+begin_comment
+comment|/**  * Represents an Effective User  *   * This is used during setUid and setGid operations  * to replace the Subject used by DBBroker  * with a subject which is potentially a composite  * of a user and/or group  */
+end_comment
 
 begin_class
 specifier|public
