68cf93ee80f5f9046d42ee58cd02ce4ed8c359ce
Dmitriy Shabanov <shabanovd@gmail.com>
2014-01-30 18:03:01 +0400

    triggers redesign and new SAXTrigger
    Former-commit-id: ea75c3c2c54f7786506e3db9b195c3b02b5a4cba

--- src/org/exist/collections/triggers/Trigger.java
+++ src/org/exist/collections/triggers/Trigger.java
@@ -1,6 +1,6 @@
 begin_unit|revision:1.0.0;language:Java;cregit-version:0.0.1
 begin_comment
-comment|/*  *  eXist Open Source Native XML Database  *  Copyright (C) 2003-2012 The eXist Project  *  http://exist-db.org  *  *  This program is free software; you can redistribute it and/or  *  modify it under the terms of the GNU Lesser General Public License  *  as published by the Free Software Foundation; either version 2  *  of the License, or (at your option) any later version.  *  *  This program is distributed in the hope that it will be useful,  *  but WITHOUT ANY WARRANTY; without even the implied warranty of  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the  *  GNU Lesser General Public License for more details.  *  *  You should have received a copy of the GNU Lesser General Public  *  License along with this library; if not, write to the Free Software  *  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA  *  *  $Id$  */
+comment|/*  *  eXist Open Source Native XML Database  *  Copyright (C) 2001-2014 The eXist Project  *  http://exist-db.org  *  *  This program is free software; you can redistribute it and/or  *  modify it under the terms of the GNU Lesser General Public License  *  as published by the Free Software Foundation; either version 2  *  of the License, or (at your option) any later version.  *  *  This program is distributed in the hope that it will be useful,  *  but WITHOUT ANY WARRANTY; without even the implied warranty of  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the  *  GNU Lesser General Public License for more details.  *  *  You should have received a copy of the GNU Lesser General Public  *  License along with this library; if not, write to the Free Software  *  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA  */
 end_comment
 
 begin_package
@@ -32,6 +32,18 @@ operator|.
 name|util
 operator|.
 name|Map
+import|;
+end_import
+
+begin_import
+import|import
+name|org
+operator|.
+name|apache
+operator|.
+name|log4j
+operator|.
+name|Logger
 import|;
 end_import
 
@@ -80,6 +92,21 @@ specifier|public
 interface|interface
 name|Trigger
 block|{
+specifier|public
+specifier|static
+specifier|final
+name|Logger
+name|LOG
+init|=
+name|Logger
+operator|.
+name|getLogger
+argument_list|(
+name|Trigger
+operator|.
+name|class
+argument_list|)
+decl_stmt|;
 specifier|public
 specifier|final
 specifier|static
@@ -189,7 +216,7 @@ block|,
 literal|"DELETE-COLLECTION"
 block|}
 decl_stmt|;
-comment|/** 	 * The configure method is called once whenever the collection configuration is loaded. Use it to 	 * initialize the trigger, probably by looking at the parameters. 	 *  	 * @param broker the database instance used to load the collection configuration. The broker object is 	 * 	required for all database actions. Please note: the broker instance used for configuration is probably 	 * 	different from the one passed to the prepare method. Don't store the broker object in your class. 	 * @param parent the collection to which this trigger belongs.  	 * @param parameters a Map containing any key/value parameters defined in the configuration file. 	 * @throws CollectionConfigurationException if the trigger cannot be initialized. 	 */
+comment|/**      * The configure method is called once whenever the collection configuration      * is loaded. Use it to initialize the trigger, probably by looking at the      * parameters.      *       * @param broker      *            the database instance used to load the collection      *            configuration. The broker object is required for all database      *            actions. Please note: the broker instance used for      *            configuration is probably different from the one passed to the      *            prepare method. Don't store the broker object in your class.      * @param parent      *            the collection to which this trigger belongs.      * @param parameters      *            a Map containing any key/value parameters defined in the      *            configuration file.      * @throws CollectionConfigurationException      *             if the trigger cannot be initialized.      */
 specifier|public
 name|void
 name|configure
