db0fff9020a013b139551a270acf2a85ebe69efa
Leif-Jöran Olsson <ljo@exist-db.org>
2014-07-16 18:25:58 +0200

    Make use of git.commit (instead of svn-revision) together with project.version and use current year for copyright in InteractiveClient.
    Former-commit-id: 094ff87831d757befece1521190819d84006ecf5

--- src/org/exist/Version.java
+++ src/org/exist/Version.java
@@ -1,6 +1,6 @@
 begin_unit|revision:1.0.0;language:Java;cregit-version:0.0.1
 begin_comment
-comment|/*  *  eXist Open Source Native XML Database  *  Copyright (C) 2010 The eXist Project  *  http://exist-db.org  *    *  This program is free software; you can redistribute it and/or  *  modify it under the terms of the GNU Lesser General Public License  *  as published by the Free Software Foundation; either version 2  *  of the License, or (at your option) any later version.  *    *  This program is distributed in the hope that it will be useful,  *  but WITHOUT ANY WARRANTY; without even the implied warranty of  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the  *  GNU Lesser General Public License for more details.  *    *  You should have received a copy of the GNU Lesser General Public License  *  along with this program; if not, write to the Free Software  *  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.  *    *  $Id$  */
+comment|/*  * eXist Open Source Native XML Database  * Copyright (C) 2010-2014 The eXist-db Project  * http://exist-db.org  *  * This program is free software; you can redistribute it and/or  * modify it under the terms of the GNU Lesser General Public License  * as published by the Free Software Foundation; either version 2  * of the License, or (at your option) any later version.  *    * This program is distributed in the hope that it will be useful,  * but WITHOUT ANY WARRANTY; without even the implied warranty of  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the  * GNU Lesser General Public License for more details.  *   * You should have received a copy of the GNU Lesser General Public License  * along with this program; if not, write to the Free Software Foundation  * Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.  *    */
 end_comment
 
 begin_package
@@ -81,7 +81,7 @@ specifier|private
 specifier|static
 specifier|final
 name|String
-name|SVN_REVISION
+name|GIT_COMMIT
 decl_stmt|;
 static|static
 block|{
@@ -145,7 +145,7 @@ argument_list|(
 literal|"product-build"
 argument_list|)
 expr_stmt|;
-name|SVN_REVISION
+name|GIT_COMMIT
 operator|=
 operator|(
 name|String
@@ -154,7 +154,7 @@ name|properties
 operator|.
 name|get
 argument_list|(
-literal|"svn-revision"
+literal|"git-commit"
 argument_list|)
 expr_stmt|;
 block|}
@@ -188,6 +188,8 @@ return|return
 name|BUILD
 return|;
 block|}
+annotation|@
+name|Deprecated
 specifier|public
 specifier|static
 name|String
@@ -195,7 +197,17 @@ name|getSvnRevision
 parameter_list|()
 block|{
 return|return
-name|SVN_REVISION
+name|GIT_COMMIT
+return|;
+block|}
+specifier|public
+specifier|static
+name|String
+name|getGitCommit
+parameter_list|()
+block|{
+return|return
+name|GIT_COMMIT
 return|;
 block|}
 block|}
