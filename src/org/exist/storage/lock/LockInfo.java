ca9e00052776b4f3e22d6fba5c100fdba65dbfca
Adam Retter <adam.retter@googlemail.com>
2015-01-28 15:04:44 +0000

    [ignore] minor cleanup
    Former-commit-id: 4e2b49fceecdbd853d67baaa3ca98c1fc56c5d68

--- src/org/exist/storage/lock/LockInfo.java
+++ src/org/exist/storage/lock/LockInfo.java
@@ -1,6 +1,6 @@
 begin_unit|revision:1.0.0;language:Java;cregit-version:0.0.1
 begin_comment
-comment|/*  *  eXist Open Source Native XML Database  *  Copyright (C) 2001-07 The eXist Project  *  http://exist-db.org  *  *  This program is free software; you can redistribute it and/or  *  modify it under the terms of the GNU Lesser General Public License  *  as published by the Free Software Foundation; either version 2  *  of the License, or (at your option) any later version.  *  *  This program is distributed in the hope that it will be useful,  *  but WITHOUT ANY WARRANTY; without even the implied warranty of  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the  *  GNU Lesser General Public License for more details.  *  *  You should have received a copy of the GNU Lesser General Public  *  License along with this library; if not, write to the Free Software  *  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA  *  * $Id$  */
+comment|/*  *  eXist Open Source Native XML Database  *  Copyright (C) 2001-2015 The eXist Project  *  http://exist-db.org  *  *  This program is free software; you can redistribute it and/or  *  modify it under the terms of the GNU Lesser General Public License  *  as published by the Free Software Foundation; either version 2  *  of the License, or (at your option) any later version.  *  *  This program is distributed in the hope that it will be useful,  *  but WITHOUT ANY WARRANTY; without even the implied warranty of  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the  *  GNU Lesser General Public License for more details.  *  *  You should have received a copy of the GNU Lesser General Public  *  License along with this library; if not, write to the Free Software  *  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA  */
 end_comment
 
 begin_package
@@ -22,6 +22,16 @@ operator|.
 name|io
 operator|.
 name|PrintStream
+import|;
+end_import
+
+begin_import
+import|import
+name|java
+operator|.
+name|util
+operator|.
+name|Arrays
 import|;
 end_import
 
@@ -331,9 +341,9 @@ name|println
 argument_list|(
 literal|"Held by: "
 operator|+
-name|DeadlockDetection
+name|Arrays
 operator|.
-name|arrayToString
+name|toString
 argument_list|(
 name|getOwners
 argument_list|()
@@ -346,9 +356,9 @@ name|println
 argument_list|(
 literal|"Read locks: "
 operator|+
-name|DeadlockDetection
+name|Arrays
 operator|.
-name|arrayToString
+name|toString
 argument_list|(
 name|getReadLocks
 argument_list|()
@@ -361,9 +371,9 @@ name|println
 argument_list|(
 literal|"Wait for read: "
 operator|+
-name|DeadlockDetection
+name|Arrays
 operator|.
-name|arrayToString
+name|toString
 argument_list|(
 name|getWaitingForRead
 argument_list|()
@@ -376,9 +386,9 @@ name|println
 argument_list|(
 literal|"Wait for write: "
 operator|+
-name|DeadlockDetection
+name|Arrays
 operator|.
-name|arrayToString
+name|toString
 argument_list|(
 name|getWaitingForWrite
 argument_list|()
