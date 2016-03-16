5e9f8e532ce3e2fc71663b30c7ade3db2a6bdd89
Adam Retter <adam.retter@googlemail.com>
2016-03-16 13:36:15 -0400

    [refactor] Improve the contract of EXistInputSource to enforce open/close semantics
    Former-commit-id: 902c42b09aff4cfccaecf1a1b392355a8a9e3ee5

--- src/org/exist/util/EXistInputSource.java
+++ src/org/exist/util/EXistInputSource.java
@@ -11,6 +11,18 @@ end_package
 
 begin_import
 import|import
+name|net
+operator|.
+name|jcip
+operator|.
+name|annotations
+operator|.
+name|NotThreadSafe
+import|;
+end_import
+
+begin_import
+import|import
 name|org
 operator|.
 name|xml
@@ -32,6 +44,8 @@ import|;
 end_import
 
 begin_class
+annotation|@
+name|NotThreadSafe
 specifier|public
 specifier|abstract
 class|class
@@ -41,6 +55,12 @@ name|InputSource
 implements|implements
 name|Closeable
 block|{
+specifier|private
+name|boolean
+name|closed
+init|=
+literal|false
+decl_stmt|;
 specifier|public
 name|EXistInputSource
 parameter_list|()
@@ -61,14 +81,63 @@ name|String
 name|getSymbolicPath
 parameter_list|()
 function_decl|;
+comment|/** 	 * Determines if the InputSource was closed 	 * 	 * @return true if the InputSource was previously closed, false otherwise 	 */
+specifier|public
+name|boolean
+name|isClosed
+parameter_list|()
+block|{
+return|return
+name|closed
+return|;
+block|}
+comment|/** 	 * Throws an exception if the InputSource is closed 	 * 	 * @throws IllegalStateException If the InputSource was previously closed 	 */
+specifier|protected
+name|void
+name|assertOpen
+parameter_list|()
+block|{
+if|if
+condition|(
+name|isClosed
+argument_list|()
+condition|)
+block|{
+throw|throw
+operator|new
+name|IllegalStateException
+argument_list|(
+literal|"The InputSource has been closed"
+argument_list|)
+throw|;
+block|}
+block|}
+comment|/** 	 * Re-Opens the InputSource by just 	 * setting the closed flag to false 	 */
+specifier|protected
+name|void
+name|reOpen
+parameter_list|()
+block|{
+name|this
+operator|.
+name|closed
+operator|=
+literal|false
+expr_stmt|;
+block|}
+comment|/** 	 * Just sets the status of the InputStream to closed 	 * 	 * Sub-classes that override this should call {@code super.close()} 	 */
 annotation|@
 name|Override
 specifier|public
-specifier|abstract
 name|void
 name|close
 parameter_list|()
-function_decl|;
+block|{
+name|closed
+operator|=
+literal|true
+expr_stmt|;
+block|}
 block|}
 end_class
 
