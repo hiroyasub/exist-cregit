5c8ffc49524896d1f9bbfff893cdcbd8f20de637
Dmitriy Shabanov <shabanovd@gmail.com>
2017-03-07 10:44:48 +0300

    raise error if xquery is < 3.1
    Former-commit-id: 3ae94ffd97536c9eca611847ed6e9285eafb5334

--- src/org/exist/xquery/ArrowOperator.java
+++ src/org/exist/xquery/ArrowOperator.java
@@ -1,4 +1,8 @@
 begin_unit|revision:1.0.0;language:Java;cregit-version:0.0.1
+begin_comment
+comment|/*  * eXist Open Source Native XML Database  * Copyright (C) 2001-2017 The eXist Project  * http://exist-db.org  *  * This program is free software; you can redistribute it and/or  * modify it under the terms of the GNU Lesser General Public License  * as published by the Free Software Foundation; either version 2  * of the License, or (at your option) any later version.  *  * This program is distributed in the hope that it will be useful,  * but WITHOUT ANY WARRANTY; without even the implied warranty of  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the  * GNU Lesser General Public License for more details.  *  * You should have received a copy of the GNU Lesser General Public  * License along with this library; if not, write to the Free Software  * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA  */
+end_comment
+
 begin_package
 package|package
 name|org
@@ -285,6 +289,31 @@ parameter_list|)
 throws|throws
 name|XPathException
 block|{
+if|if
+condition|(
+name|getContext
+argument_list|()
+operator|.
+name|getXQueryVersion
+argument_list|()
+operator|<
+literal|31
+condition|)
+block|{
+throw|throw
+operator|new
+name|XPathException
+argument_list|(
+name|this
+argument_list|,
+name|ErrorCodes
+operator|.
+name|EXXQDY0003
+argument_list|,
+literal|"arrow operator is not available before XQuery 3.1"
+argument_list|)
+throw|;
+block|}
 name|this
 operator|.
 name|cachedContextInfo
