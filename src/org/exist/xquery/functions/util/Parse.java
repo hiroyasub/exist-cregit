f52441dcbbd78c7378f26ebab17fd21e9ed5d307
Duncan Paterson <d.paterson@me.com>
2018-06-16 13:07:44 +0200

    add deprecation warning
    Former-commit-id: 76b4d9a65cff13aad4c4082670ee40a0e522654d

--- src/org/exist/xquery/functions/util/Parse.java
+++ src/org/exist/xquery/functions/util/Parse.java
@@ -1,6 +1,6 @@
 begin_unit|revision:1.0.0;language:Java;cregit-version:0.0.1
 begin_comment
-comment|/*  *  eXist Open Source Native XML Database  *  Copyright (C) 2001-09 The eXist Project  *  http://exist-db.org  *    *  This program is free software; you can redistribute it and/or  *  modify it under the terms of the GNU Lesser General Public License  *  as published by the Free Software Foundation; either version 2  *  of the License, or (at your option) any later version.  *    *  This program is distributed in the hope that it will be useful,  *  but WITHOUT ANY WARRANTY; without even the implied warranty of  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the  *  GNU Lesser General Public License for more details.  *    *  You should have received a copy of the GNU Lesser General Public License  *  along with this program; if not, write to the Free Software  *  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.  *    *  $Id$  */
+comment|/*  *  eXist Open Source Native XML Database  *  Copyright (C) 2001-09 The eXist Project  *  http://exist-db.org  *  *  This program is free software; you can redistribute it and/or  *  modify it under the terms of the GNU Lesser General Public License  *  as published by the Free Software Foundation; either version 2  *  of the License, or (at your option) any later version.  *  *  This program is distributed in the hope that it will be useful,  *  but WITHOUT ANY WARRANTY; without even the implied warranty of  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the  *  GNU Lesser General Public License for more details.  *  *  You should have received a copy of the GNU Lesser General Public License  *  along with this program; if not, write to the Free Software  *  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.  *  *  $Id$  */
 end_comment
 
 begin_package
@@ -409,6 +409,7 @@ operator|.
 name|class
 argument_list|)
 decl_stmt|;
+comment|/** 		* @deprecated use parse-xml instead 		*/
 specifier|public
 specifier|final
 specifier|static
@@ -434,6 +435,10 @@ operator|.
 name|PREFIX
 argument_list|)
 argument_list|,
+literal|"!This function will be deprecated in a future version!"
+operator|+
+literal|"use parse-xml instead."
+operator|+
 literal|"Parses the passed string value into an XML fragment. The string has to be "
 operator|+
 literal|"well-formed XML. An empty sequence is returned if the argument is an "
