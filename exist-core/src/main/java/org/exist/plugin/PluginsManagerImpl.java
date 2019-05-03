1b8755340bc95eb676b7a7de53a659f2499646a1
Adam Retter <adam.retter@googlemail.com>
2019-05-04 00:00:25 +0400

    [refactor] Remove reference to non-existent class
    Former-commit-id: b40bc765a5a6ec32e62b8ecf74062f4354a6d2d3

--- exist-core/src/main/java/org/exist/plugin/PluginsManagerImpl.java
+++ exist-core/src/main/java/org/exist/plugin/PluginsManagerImpl.java
@@ -581,11 +581,6 @@ argument_list|(
 literal|"org.exist.scheduler.SchedulerManager"
 argument_list|)
 expr_stmt|;
-name|addPlugin
-argument_list|(
-literal|"org.exist.monitoring.MonitoringManager"
-argument_list|)
-expr_stmt|;
 block|}
 annotation|@
 name|Override
